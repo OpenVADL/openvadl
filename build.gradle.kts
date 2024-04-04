9966d8e000788ca171a935b26ac473d736f005be
Johannes Zottele <official@johannes-zottele.at>
2024-04-04 13:24:01 +0200

    gradle: Adjust build configs

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+
+plugins {
+    id("java")
+    id("net.ltgt.errorprone") version "3.1.0"
+}
+
+group = "ord.vadl"
+version = "1.0-SNAPSHOT"
+
+val errorProneVersion = "2.26.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    errorprone("com.uber.nullaway:nullaway:0.10.25")
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
+    errorprone("com.google.errorprone:error_prone_core:$errorProneVersion")
+    compileOnly("com.google.errorprone:error_prone_annotations:$errorProneVersion")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+
+//    options.compilerArgs.plusAssign("--enable-preview")
+
+    if (!name.lowercase().contains("test")) {
+        options.errorprone {
+            check("NullAway", CheckSeverity.ERROR)
+            option("NullAway:AnnotatedPackages", "at.ac.tuwien.complang.vadl")
+            disable("EqualsGetClass")
+        }
+    }
+}
+
+tasks {
+    compileTestJava {
+        options.errorprone.isEnabled.set(false)
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
