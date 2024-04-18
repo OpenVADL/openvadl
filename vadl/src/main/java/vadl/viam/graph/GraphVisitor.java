77500e7b1dddc0ec0bcb64d379c6e996eac47e06
Johannes Zottele <official@johannes-zottele.at>
2024-04-18 18:04:18 +0200

    viam: Merge java-annotations update

--- vadl/src/main/java/vadl/viam/graph/GraphVisitor.java
+++ vadl/src/main/java/vadl/viam/graph/GraphVisitor.java
@@ -3,7 +3,8 @@ package vadl.viam.graph;
 import javax.annotation.Nullable;
 
 /**
- * The GraphVisitor interface represents a visitor that can visit nodes in a graph and obtain a result of type R.
+ * The GraphVisitor interface represents a visitor that can visit nodes in a graph and
+ * obtain a result of type R.
  *
  * @param <R> the type of the result obtained from visiting the graph nodes
  */
@@ -13,7 +14,8 @@ public interface GraphVisitor<R> {
   R visit(Node from, @Nullable Node to);
 
   /**
-   * The Applier interface represents a graph visitor that assigns new values to inputs of a node in a graph.
+   * The Applier interface represents a graph visitor that assigns new values to inputs of
+   * a node in a graph.
    *
    * @param <R> the type of the result that gets assigned to the node's inputs
    */
@@ -24,6 +26,17 @@ public interface GraphVisitor<R> {
       return applyNullable(from, to);
     }
 
+    /**
+     * Applies a transformation to an edge and returns the result as an instance of the
+     * specified class.
+     *
+     * @param from  the start of the edge (not input node)
+     * @param to    the end of the edge (input node)
+     * @param clazz the class representing the desired type of the result
+     * @param <T>   the type of the result
+     * @return the result that is applied to the {@code to} node
+     * @throws ViamGraphError if the applier produces invalid node type
+     */
     default <T extends Node> T apply(Node from, @Nullable Node to, Class<T> clazz) {
       var newNode = apply(from, to);
       newNode.ensure(clazz.isInstance(newNode), "unable to apply %s to %s.", newNode.getClass(),
@@ -31,6 +44,14 @@ public interface GraphVisitor<R> {
       return clazz.cast(newNode);
     }
 
+    /**
+     * Applies a transformation to the given nodes and returns the result.
+     *
+     * @param from the start of the edge (not input node)
+     * @param to   the end of the edge (input node)
+     * @return the result of the transformation
+     * @throws ViamGraphError if the applier produces a null node
+     */
     default R apply(Node from, @Nullable Node to) {
       var newNode = applyNullable(from, to);
       ViamGraphError.ensureNonNull(newNode, from.graph(), from, to,
