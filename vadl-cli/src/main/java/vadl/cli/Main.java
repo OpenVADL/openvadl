dfe1164c98c5fe22f288df0ee7b7e0d454c4d3f5
Johannes Zottele <official@johannes-zottele.at>
2024-04-24 22:00:44 +0200

    ci: Fix failure reasons

--- vadl-cli/src/main/java/vadl/cli/Main.java
+++ vadl-cli/src/main/java/vadl/cli/Main.java
@@ -1,16 +1,10 @@
 package vadl.cli;
 
-import javax.annotation.Nullable;
-
 /**
- * The VADL CLI entry class
+ * The VADL CLI entry class.
  */
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-  }
-
-  public boolean test(@Nullable String t) {
-    return t.contains("s");
   }
 }
