4e04beea40460df4856e3e828a3f0e97c2aed3bf
Johannes Zottele <official@johannes-zottele.at>
2024-04-26 18:37:32 +0200

    viam: Add InstrParamNode

---
+++ vadl/src/main/java/vadl/viam/graph/nodes/dependency/ParamNode.java
@@ -0,0 +1,11 @@
+package vadl.viam.graph.nodes.dependency;
+
+import vadl.viam.graph.nodes.DependencyNode;
+
+/**
+ * Represents an abstract parameter node, like an instruction or function
+ * parameter.
+ */
+public abstract class ParamNode extends DependencyNode {
+
+}
