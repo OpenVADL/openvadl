53fea80b80f35f9e299b49c44f4fefbd6eefbcf4
Johannes Zottele <official@johannes-zottele.at>
2024-04-26 10:21:32 +0200

    viam: Add SourceLocation source extraction

--- vadl/src/main/java/vadl/utils/SourceLocation.java
+++ vadl/src/main/java/vadl/utils/SourceLocation.java
@@ -1,6 +1,14 @@
 package vadl.utils;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import javax.annotation.Nullable;
 
 public record SourceLocation(
     URI uri,
@@ -28,6 +36,37 @@ public record SourceLocation(
         + this.begin
         + " .. "
         + this.end;
+  }
+
+  public @Nullable String toSourceString() {
+    try (Stream<String> lines = Files.lines(Paths.get(uri))) {
+      if (begin.line <= 0) {
+        return null;
+      }
+
+      var lineDiff = end.line - begin.line;
+      var sourceLines = lines.skip(begin.line - 1).limit(lineDiff + 1)
+          .collect(Collectors.toCollection(ArrayList::new));
+
+      var lineNumber = sourceLines.size();
+      return IntStream.range(0, lineNumber)
+          .mapToObj(i -> {
+            var line = sourceLines.get(i);
+            if (i == lineNumber - 1 && end.column != -1) {
+              line = line.substring(0, end.column - 1);
+            }
+
+            if (i == 0 && begin.column != -1) {
+              line = line.substring(begin.column - 1);
+            }
+            return line;
+          })
+          .collect(Collectors.joining("\n"));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 
   @Override
