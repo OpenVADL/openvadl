1745b82280b2b16f994ff6c561ef64119e50ced2
Johannes Zottele <official@johannes-zottele.at>
2024-04-30 12:59:31 +0200

    viam: ReadRegNode add register dummy

--- vadl/src/main/java/vadl/viam/graph/dependency/ReadRegNode.java
+++ vadl/src/main/java/vadl/viam/graph/dependency/ReadRegNode.java
@@ -1,11 +1,25 @@
 package vadl.viam.graph.dependency;
 
+import java.util.List;
+import vadl.javaannotations.viam.DataValue;
+
 /**
  * The ReadRegNode class is a subclass of ReadNode that represents
  * a node that reads a value from a register location.
  */
 public class ReadRegNode extends ReadNode {
-  public ReadRegNode(ExpressionNode location) {
+
+  @DataValue
+  protected String register; // TODO: Replace by proper datastructure
+
+  public ReadRegNode(String register, ExpressionNode location) {
     super(location);
+    this.register = register;
+  }
+
+  @Override
+  protected void collectData(List<Object> collection) {
+    super.collectData(collection);
+    collection.add(register);
   }
 }
