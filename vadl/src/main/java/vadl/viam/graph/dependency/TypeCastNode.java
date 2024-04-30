1fc07b5a6ed8521b43c4bbb227d1fb85717869a3
Johannes Zottele <official@johannes-zottele.at>
2024-04-30 11:42:29 +0200

    viam: Rename CastNode to TypeCastNode

--- vadl/src/main/java/vadl/viam/graph/dependency/TypeCastNode.java
+++ vadl/src/main/java/vadl/viam/graph/dependency/TypeCastNode.java
@@ -3,12 +3,12 @@ package vadl.viam.graph.dependency;
 import java.util.List;
 import vadl.javaannotations.viam.DataValue;
 
-public class CastNode extends UnaryNode {
+public class TypeCastNode extends UnaryNode {
 
   @DataValue
   private final String type; // TODO: Refactor by correct datastructure
 
-  public CastNode(ExpressionNode value, String type) {
+  public TypeCastNode(ExpressionNode value, String type) {
     super(value);
     this.type = type;
   }
