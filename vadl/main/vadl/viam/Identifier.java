3932b5fae2e69812c4c3e5d1319797652e943cab
Johannes Zottele <official@johannes-zottele.at>
2024-05-07 17:15:29 +0200

    viam: Extend Identifier by parts

--- vadl/main/vadl/viam/Identifier.java
+++ vadl/main/vadl/viam/Identifier.java
@@ -6,8 +6,29 @@ import vadl.utils.SourceLocation;
  * Source level identifier class.
  */
 public record Identifier(
-    String name,
+    String[] parts,
     SourceLocation sourceLocation
 ) {
+
+  Identifier(String name, SourceLocation sourceLocation) {
+    this(new String[] {name}, sourceLocation);
+  }
+
+  Identifier(SourceLocation sourceLocation, String... parts) {
+    this(parts, sourceLocation);
+  }
+
+  @Override
+  public String toString() {
+    return String.join("::", this.parts);
+  }
+
+  public String name() {
+    return String.join("::", this.parts);
+  }
+
+  public String simpleName() {
+    return this.parts[this.parts.length - 1];
+  }
 
 }
