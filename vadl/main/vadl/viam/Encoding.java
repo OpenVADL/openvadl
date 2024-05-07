fd9e6d5781c4ebc01963cc7c9bdaebc5555c9825
Johannes Zottele <official@johannes-zottele.at>
2024-05-07 17:10:23 +0200

    viam: Add Encoding and Field class

--- vadl/main/vadl/viam/Encoding.java
+++ vadl/main/vadl/viam/Encoding.java
@@ -1,8 +1,148 @@
 package vadl.viam;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import vadl.types.Type;
+
+/**
+ * The encoding for a specific instruction.
+ *
+ * <p>It holds instruction encoded fields available to the instruction.
+ * Each field has a bit range and type that specifies the location.
+ * Optionally a field also has a constant value if it is known in advance.</p>
+ */
 public class Encoding extends Definition {
 
-  public Encoding(Identifier identifier) {
+  private final Instruction instruction;
+  private final List<Field> fields = new ArrayList<Field>();
+
+  public Encoding(Identifier identifier, Instruction instruction) {
     super(identifier);
+    this.instruction = instruction;
   }
+
+  public Encoding addFields(Field... encodingFields) {
+    fields.addAll(List.of(encodingFields));
+    return this;
+  }
+
+  public Instruction instruction() {
+    return instruction;
+  }
+
+
+  /**
+   * A field within an encoding.
+   * Holds information about the type, ranges, and value of the field.
+   */
+  public static class Field extends Definition {
+
+    private final Type type;
+    private final List<Constant.Range> ranges;
+    private @Nullable Constant value;
+
+    private final Encoding encoding;
+
+    /**
+     * Constructs a Field object with the given identifier, type, ranges, and encoding.
+     *
+     * @param identifier the identifier of the field
+     * @param type       the type of the field
+     * @param ranges     the list of constant ranges
+     * @param encoding   the parent encoding of the field
+     */
+    public Field(
+        Identifier identifier,
+        Type type,
+        List<Constant.Range> ranges,
+        Encoding encoding) {
+      super(identifier);
+
+      ViamError.ensure(ranges.stream()
+              .map(Constant::type)
+              .collect(Collectors.toSet()).size() == 1,
+          "Ranges must be of same type: %s", ranges
+      );
+
+      this.type = type;
+      ranges.sort(Comparator.comparing(a -> a.from().value()));
+      this.ranges = ranges;
+      this.encoding = encoding;
+    }
+
+    /**
+     * Constructs a Field object with the given identifier, type, ranges, and encoding.
+     *
+     * @param identifier the identifier of the field
+     * @param type       the type of the field
+     * @param range      the constant range
+     * @param encoding   the parent encoding of the field
+     */
+    public Field(
+        Identifier identifier,
+        Type type,
+        Constant.Range range,
+        Encoding encoding) {
+      this(identifier, type, List.of(range), encoding);
+    }
+
+    public void setValue(Constant value) {
+      ViamError.ensure(value.type().equals(type), "Value must be of type %s, was %s", type,
+          value.type());
+      this.value = value;
+    }
+
+    @Nullable
+    public Constant value() {
+      return value;
+    }
+
+    public boolean hasValue() {
+      return value != null;
+    }
+
+    public List<Constant.Range> ranges() {
+      return ranges;
+    }
+
+    public Type type() {
+      return type;
+    }
+
+    public Encoding encoding() {
+      return encoding;
+    }
+
+
+    public int size() {
+      return ranges
+          .stream()
+          .mapToInt(Constant.Range::size)
+          .sum();
+    }
+
+    /**
+     * Converts the occupation ranges to an array of integers.
+     * The resulting array contains the indices of all bits, that are
+     * occupied by this field.
+     *
+     * @return an array of integers representing the occupation ranges
+     */
+    public int[] toOccupationArray() {
+      var arr = new int[size()];
+      int index = 0;
+      for (Constant.Range range : ranges) {
+        List<Constant.Value> values = range.toList();
+        for (Constant.Value value : values) {
+          arr[index++] = value.value().intValue();
+        }
+      }
+      return arr;
+    }
+
+  }
+
 }
