406343b75dee1a12b388757b7fcc8a30add51390
Johannes Zottele <official@johannes-zottele.at>
2024-05-08 11:43:16 +0200

    viam: Add toString for Encoding and Encoding.Field

--- vadl/main/vadl/viam/Function.java
+++ vadl/main/vadl/viam/Function.java
@@ -5,20 +5,20 @@ import vadl.types.Type;
 import vadl.viam.graph.Graph;
 
 public class Function extends Definition {
-  private final Graph behaviour;
+  private final Graph behavior;
   private final Type returnType;
   private final List<Parameter> arguments;
 
   public Function(Identifier identifier, List<Parameter> arguments,
                   Type returnType) {
     super(identifier);
-    this.behaviour = new Graph(identifier.name());
+    this.behavior = new Graph(identifier.name());
     this.returnType = returnType;
     this.arguments = arguments;
   }
 
-  public Graph behaviour() {
-    return behaviour;
+  public Graph behavior() {
+    return behavior;
   }
 
   public List<Parameter> arguments() {
