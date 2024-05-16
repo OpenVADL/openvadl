58c3a8b3dd41b4cead0ac0d711719a5b47091632
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-05-16 11:43:40 +0200

    viam: Add value to ReturnNode

--- vadl/main/vadl/viam/graph/control/ReturnNode.java
+++ vadl/main/vadl/viam/graph/control/ReturnNode.java
@@ -1,6 +1,7 @@
 package vadl.viam.graph.control;
 
 import vadl.viam.graph.NodeList;
+import vadl.viam.graph.dependency.ExpressionNode;
 import vadl.viam.graph.dependency.SideEffectNode;
 
 
@@ -8,7 +9,11 @@ import vadl.viam.graph.dependency.SideEffectNode;
  * Represents the end node of the control flow graph of a pure function.
  */
 public class ReturnNode extends AbstractEndNode {
-  public ReturnNode(NodeList<SideEffectNode> sideEffects) {
+
+  public ExpressionNode value;
+
+  public ReturnNode(ExpressionNode value, NodeList<SideEffectNode> sideEffects) {
     super(sideEffects);
+    this.value = value;
   }
 }
