dfd4f842aefbe7b44f79e574205cf4596e82a626
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-05-20 13:38:42 +0200

    viam: Add String type

---
+++ vadl/main/vadl/types/StringType.java
@@ -0,0 +1,26 @@
+package vadl.types;
+
+public class StringType extends DataType {
+
+  private static final int charWidth = 8;
+  private final int size;
+
+  protected StringType(int size) {
+    this.size = size;
+  }
+
+  @Override
+  public int bitWidth() {
+    return size * charWidth;
+  }
+
+  @Override
+  public boolean canBeCastTo(DataType other) {
+    return false;
+  }
+
+  @Override
+  public String name() {
+    return "String<" + size + ">";
+  }
+}
