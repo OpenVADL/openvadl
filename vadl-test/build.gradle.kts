8ba2a4a15ac73c2a7a35fec26f1c1a983e480b14
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-06-20 09:43:36 +0200

    viam: Setup vadl-test module

---
+++ vadl-test/build.gradle.kts
@@ -0,0 +1,30 @@
+import net.ltgt.gradle.errorprone.errorprone
+
+plugins {
+    id("java")
+}
+
+group = "vadl"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation(project(":vadl"))
+
+    implementation(platform("org.junit:junit-bom:5.10.2"))
+    implementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.junit.platform:junit-platform-launcher:1.10.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks.withType<JavaCompile> {
+    options.errorprone.isEnabled.set(false)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
