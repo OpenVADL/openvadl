4032c677096910d22e14600d49679c034357a5d0
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-06-21 12:39:00 +0200

    viam: Fix part normalization of identifier parts

--- vadl/main/vadl/viam/Identifier.java
+++ vadl/main/vadl/viam/Identifier.java
@@ -13,12 +13,21 @@ public record Identifier(
     SourceLocation sourceLocation
 ) {
 
+  public Identifier(String[] parts, SourceLocation sourceLocation) {
+    for (int i = 0; i < parts.length; i++) {
+      parts[i] = normalizePart(parts[i]);
+    }
+    this.parts = parts;
+    this.sourceLocation = sourceLocation;
+  }
+
   public Identifier(String name, SourceLocation sourceLocation) {
-    this(new String[] {name}, sourceLocation);
+    this(List.of(name), sourceLocation);
   }
 
   public Identifier(List<String> parts, SourceLocation sourceLocation) {
-    this(parts.toArray(String[]::new), sourceLocation);
+    this(parts.toArray(String[]::new),
+        sourceLocation);
   }
 
   /**
@@ -88,6 +97,17 @@ public record Identifier(
 
   public String simpleName() {
     return this.parts[this.parts.length - 1];
+  }
+
+  private static String normalizePart(String part) {
+    part = part.trim();
+    if (part.startsWith(".")) {
+      part = part.substring(1);
+    }
+    if (part.endsWith(".")) {
+      part = part.substring(0, part.length() - 1);
+    }
+    return part;
   }
 
 }
