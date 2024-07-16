78086038d96f9b5c2d110978c7c6817bf410fc04
Kevin Per <kevin.per@protonmail.com>
2024-07-16 16:17:12 +0200

    oop: Make Checkstyle happy

--- vadl/test/vadl/oop/SymbolTableTest.java
+++ vadl/test/vadl/oop/SymbolTableTest.java
@@ -1,9 +1,8 @@
 package vadl.oop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SymbolTableTest {
   @Test
