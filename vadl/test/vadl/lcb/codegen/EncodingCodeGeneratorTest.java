66e72dd8b69cd5111440d830b9f8a579fa0ca200
Kevin Per <kevin.per@protonmail.com>
2024-07-19 13:01:07 +0200

    chore: Make Checkstyle happy

--- vadl/test/vadl/lcb/codegen/EncodingCodeGeneratorTest.java
+++ vadl/test/vadl/lcb/codegen/EncodingCodeGeneratorTest.java
@@ -1,6 +1,6 @@
 package vadl.lcb.codegen;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import vadl.AbstractTest;
