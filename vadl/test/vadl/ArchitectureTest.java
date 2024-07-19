c585bfca09fe2157cf1de6414b81c53168f30a22
Kevin Per <kevin.per@protonmail.com>
2024-07-19 14:27:07 +0200

    chore: Create architecture test

---
+++ vadl/test/vadl/ArchitectureTest.java
@@ -0,0 +1,27 @@
+package vadl;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.library.Architectures;
+import org.junit.jupiter.api.Test;
+
+public class ArchitectureTest {
+  @Test
+  void shouldComplyWithArchitectureDesign() {
+    JavaClasses jc = new ClassFileImporter()
+        .importPackages("vadl");
+    Architectures.LayeredArchitecture layeredArchitecture = layeredArchitecture()
+        .consideringOnlyDependenciesInLayers()
+        .layer("Ast").definedBy("..ast..")
+        .layer("Viam").definedBy("..viam..")
+        .layer("Gcb").definedBy("..gcb..")
+        .layer("Lcb").definedBy("..lcb..")
+        .whereLayer("Ast").mayNotAccessAnyLayer()
+        .whereLayer("Viam").mayOnlyAccessLayers("Ast")
+        .whereLayer("Lcb").mayOnlyAccessLayers("Gcb", "Viam");
+
+    layeredArchitecture.check(jc);
+  }
+}
