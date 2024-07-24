b3ed54dfdf7791818c3ae500183ce866b678fb3d
Kevin Per <kevin.per@protonmail.com>
2024-07-24 17:25:06 +0200

    lcb: Abstract test functionality into new class

--- vadl/test/vadl/lcb/codegen/EncodingCodeGeneratorVerificationTest.java
+++ vadl/test/vadl/lcb/codegen/EncodingCodeGeneratorVerificationTest.java
@@ -1,34 +1,15 @@
 package vadl.lcb.codegen;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Bind;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.time.Duration;
-import java.util.Objects;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.utility.MountableFile;
-import vadl.AbstractTest;
+import vadl.DockerExecutionTest;
 import vadl.gcb.passes.encoding.strategies.EncodingGenerationStrategy;
 import vadl.gcb.passes.encoding.strategies.impl.ArithmeticImmediateStrategy;
 import vadl.gcb.passes.encoding.strategies.impl.ShiftedImmediateStrategy;
@@ -47,12 +28,23 @@ import vadl.viam.graph.dependency.ConstantNode;
 import vadl.viam.graph.dependency.FieldRefNode;
 import vadl.viam.graph.dependency.TypeCastNode;
 
-public class EncodingCodeGeneratorVerificationTest extends AbstractTest {
+public class EncodingCodeGeneratorVerificationTest extends DockerExecutionTest {
   private static final Logger logger =
       LoggerFactory.getLogger(EncodingCodeGeneratorVerificationTest.class);
 
   private static final String GENERIC_FIELD_NAME = "x";
   private static final String ENCODING_FUNCTION_NAME = "f_x";
+  private static final String MOUNT_PATH = "/app/main.py";
+
+  private static final ImageFromDockerfile DOCKER_IMAGE = new ImageFromDockerfile()
+      .withDockerfileFromBuilder(builder ->
+          builder
+              .from("python:3.8")
+              .run("python3 -m pip install z3 z3-solver")
+              .cmd("python3", "/app/main.py")
+              .build());
+  private static final String TEMPFILE_PREFIX = "encoding-z3";
+  private static final String TEMPFILE_SUFFIX = "py";
 
   private static Stream<Arguments> createFieldAccessFunctions() {
     return Stream.of(
@@ -74,7 +66,7 @@ public class EncodingCodeGeneratorVerificationTest extends AbstractTest {
   @ParameterizedTest
   @MethodSource("createFieldAccessFunctions")
   void verifyStrategies(Function encodingFunction, EncodingGenerationStrategy strategy)
-      throws IOException, InterruptedException {
+      throws IOException {
     // Setup decoding
     var fieldAccess = new Format.FieldAccess(createIdentifier("fieldAccessIdentifierValue"),
         encodingFunction, null, null);
@@ -117,45 +109,7 @@ public class EncodingCodeGeneratorVerificationTest extends AbstractTest {
         generatedDecodeFunctionCode,
         generatedEncodeWithDecodeFunctionCode);
     logger.atDebug().log(z3Code);
-
-    var tempFile = writeCodeIntoTempFile(z3Code);
-    runContainer(tempFile);
-  }
-
-  private void runContainer(File tempFile) {
-    try (GenericContainer<?> container = new GenericContainer<>(
-        new ImageFromDockerfile()
-            .withDockerfileFromBuilder(builder ->
-                builder
-                    .from("python:3.8")
-                    .run("python3 -m pip install z3 z3-solver")
-                    .cmd("python3", "/app/main.py")
-                    .build())).withCopyFileToContainer(
-        MountableFile.forHostPath(tempFile.getPath()), "/app/main.py")) {
-      container.start();
-
-      await()
-          .atMost(Duration.ofSeconds(10))
-          .until(() -> {
-            var result =
-                container.getDockerClient().inspectContainerCmd(container.getContainerId());
-            var state = result.exec().getState();
-            return state.getStatus().equals("exited");
-          });
-
-      var result = container.getDockerClient().inspectContainerCmd(container.getContainerId());
-      var state = result.exec().getState();
-      assertThat(state.getExitCodeLong()).isZero();
-    }
-  }
-
-  private File writeCodeIntoTempFile(String z3Code) throws IOException {
-    var tempFile = File.createTempFile("encoding-z3", "py");
-    tempFile.deleteOnExit();
-    var writer = new FileWriter(tempFile);
-    writer.write(z3Code);
-    writer.close();
-    return tempFile;
+    runContainerWithContent(DOCKER_IMAGE, z3Code, MOUNT_PATH, TEMPFILE_PREFIX, TEMPFILE_SUFFIX);
   }
 
   private static Function createUnsignedInt32DecodingFunction() {
