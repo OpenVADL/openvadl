fd58dffe30d56976820086ff14d8ca4303bcd318
Kevin Per <kevin.per@protonmail.com>
2024-07-25 14:34:39 +0200

    lcb: Extend encoding code generator with upcasted types

--- vadl/main/vadl/oop/CppTypeMap.java
+++ vadl/main/vadl/oop/CppTypeMap.java
@@ -1,5 +1,6 @@
 package vadl.oop;
 
+import org.checkerframework.checker.guieffect.qual.UI;
 import vadl.types.BoolType;
 import vadl.types.SIntType;
 import vadl.types.Type;
@@ -16,6 +17,8 @@ public class CppTypeMap {
   public static String getCppTypeNameByVadlType(Type type) {
     if (type instanceof BoolType) {
       return "bool";
+    } else if (type instanceof SIntType && ((SIntType) type).bitWidth() == 1) {
+      return "bool";
     } else if (type instanceof SIntType && ((SIntType) type).bitWidth() == 8) {
       return "int8_t";
     } else if (type instanceof SIntType && ((SIntType) type).bitWidth() == 16) {
@@ -24,6 +27,10 @@ public class CppTypeMap {
       return "int32_t";
     } else if (type instanceof SIntType && ((SIntType) type).bitWidth() == 64) {
       return "int64_t";
+    } else if (type instanceof SIntType && ((SIntType) type).bitWidth() == 128) {
+      return "int128_t";
+    } else if (type instanceof UIntType && ((UIntType) type).bitWidth() == 1) {
+      return "bool";
     } else if (type instanceof UIntType && ((UIntType) type).bitWidth() == 8) {
       return "uint8_t";
     } else if (type instanceof UIntType && ((UIntType) type).bitWidth() == 16) {
@@ -32,6 +39,8 @@ public class CppTypeMap {
       return "uint32_t";
     } else if (type instanceof UIntType && ((UIntType) type).bitWidth() == 64) {
       return "uint64_t";
+    } else if (type instanceof UIntType && ((UIntType) type).bitWidth() == 128) {
+      return "uint128_t";
     }
 
     throw new RuntimeException("not implemented");
