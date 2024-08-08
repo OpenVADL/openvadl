03d520424d3f9f1bedd2701a511d176967df4f2f
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-08-08 20:25:10 +0200

    viam: Add UnaryNode#replaceByNothingAndDelete

--- vadl/main/vadl/viam/graph/dependency/UnaryNode.java
+++ vadl/main/vadl/viam/graph/dependency/UnaryNode.java
@@ -29,6 +29,37 @@ public abstract class UnaryNode extends ExpressionNode {
     return this.value;
   }
 
+
+  /**
+   * Will remap the edges between usages and inputs in a way that this node isn't used
+   * anymore.
+   *
+   * <p>Visually this can be represented as<pre>
+   *            Input                           Input
+   *              |                               |
+   *              |                              /|\
+   *          This Node       --------->        / | \
+   *           /  |  \                         /  |  \
+   *          /   |   \                       /   |   \
+   *    User-1   ...   User-N           User-1   ...   User-N*
+   *
+   * </pre>
+   */
+  public void replaceByNothingAndDelete() {
+    ensure(!isDeleted(), "Replacing a deleted node is not allowed.");
+
+    // direct user's inputs to the input of this
+    // and set the users as usages of this value/input.
+    for (var user : usages().toList()) {
+      user.replaceInput(this, value);
+    }
+    // remove this node from the list of usages of value
+    value.removeUsage(this);
+    // delete this node
+    safeDelete();
+  }
+
+
   @Override
   protected void collectInputs(List<Node> collection) {
     super.collectInputs(collection);
