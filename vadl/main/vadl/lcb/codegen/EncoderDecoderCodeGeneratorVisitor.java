ee6061d66873e16e90566e7a80738aa596a1d73a
Kevin Per <kevin.per@protonmail.com>
2024-08-11 13:31:06 +0200

    chore: Make CheckStyle happy

--- vadl/main/vadl/lcb/codegen/EncoderDecoderCodeGeneratorVisitor.java
+++ vadl/main/vadl/lcb/codegen/EncoderDecoderCodeGeneratorVisitor.java
@@ -3,8 +3,8 @@ package vadl.lcb.codegen;
 import static vadl.cpp_codegen.CppTypeMap.getCppTypeNameByVadlType;
 
 import java.io.StringWriter;
-import vadl.cpp_codegen.GenericCppCodeGeneratorVisitor;
 import vadl.cpp_codegen.CppCodeGenGraphNodeVisitor;
+import vadl.cpp_codegen.GenericCppCodeGeneratorVisitor;
 import vadl.cpp_codegen.passes.type_normalization.UpcastedTypeCastNode;
 import vadl.types.BitsType;
 import vadl.types.BoolType;
