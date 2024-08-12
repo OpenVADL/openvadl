dc41fce6a3d5a1e9429e0ca0d42f538a3623329e
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-08-12 20:49:02 +0200

    viam: Add relocation tests

---
+++ vadl-test/main/vadl/test/viam/RelocationTest.java
@@ -0,0 +1,30 @@
+package vadl.test.viam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static vadl.test.TestUtils.findDefinitionByNameIn;
+
+import org.junit.jupiter.api.Test;
+import vadl.test.AbstractTest;
+import vadl.viam.Function;
+import vadl.viam.InstructionSetArchitecture;
+import vadl.viam.Relocation;
+import vadl.viam.passes.verification.ViamVerifier;
+
+public class RelocationTest extends AbstractTest {
+
+  @Test
+  void testRelocation() {
+    var spec = runAndGetViamSpecification("creation/relocation/valid_relocations.vadl");
+    ViamVerifier.verifyAllIn(spec);
+
+    var test = findDefinitionByNameIn("Test", spec, InstructionSetArchitecture.class);
+    var r1 = findDefinitionByNameIn("Test::R1", spec, Relocation.class);
+    var f1 = findDefinitionByNameIn("Test::F1", spec, Function.class);
+
+    assertEquals(1, test.relocations().size());
+    assertEquals(r1, test.relocations().get(0));
+    assertEquals(1, test.functions().size());
+    assertEquals(f1, test.functions().get(0));
+  }
+
+}
