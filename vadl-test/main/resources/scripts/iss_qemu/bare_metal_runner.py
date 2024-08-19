f98b03512508e0fd21af1ea27da8e1e411443f5f
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-08-19 12:02:55 +0200

    qemu-testing: Advanced test infrastructure

--- vadl-test/main/resources/scripts/iss_qemu/bare_metal_runner.py
+++ vadl-test/main/resources/scripts/iss_qemu/bare_metal_runner.py
@@ -1,10 +1,21 @@
 import time
 import asyncio
-from test_case import TestCase
+import traceback
+
+from test_case import TestCase, TestSpec
 
 
 async def main():
-    test_cases = [TestCase(i, "hello64") for i in range(500)]
+    test_specs = [TestSpec(
+        id=str(i), reg_tests={'a0': 'Abcdef9876543210'},
+        asm_core="""
+        li a0, 0xABCDEF9876543210
+        li a1, 0x10000000
+        li a2, 0xFFFFFF0001000FFF
+        """
+
+    ) for i in range(1)]
+    test_cases = [TestCase(spec, 1200 + i) for (i, spec) in enumerate(test_specs)]
     start_time = time.time()
 
     # Create a semaphore to limit concurrent test cases
@@ -12,8 +23,20 @@ async def main():
 
     async def run_test(test_case):
         async with semaphore:
-            print(f"Start {test_case.test_bin} for test case {test_case.id}...")
-            await test_case.run()
+            print(f"Start test case {test_case.spec.id}...")
+            start_time = time.time()
+            try:
+                await test_case.compile_and_link()
+                await test_case.exec()
+            except Exception as e:
+                test_case.test_result.status = 'FAIL'
+                test_case.test_result.errors.append(repr(e))
+                print(traceback.format_exc())
+            finally:
+                end_time = time.time()
+                test_case.test_result.duration = f"{(end_time - start_time) * 1000:.2f}ms"
+                print(f"Finish test case {test_case.spec.id} in {test_case.test_result.duration}")
+                await test_case.emit_result(dir="results", prefix="result-")
 
     # Create tasks with semaphore-controlled concurrency
     tasks = [asyncio.create_task(run_test(test_case)) for test_case in test_cases]
--- vadl-test/main/resources/scripts/iss_qemu/test_case/__init__.py
+++ vadl-test/main/resources/scripts/iss_qemu/test_case/__init__.py
@@ -1,107 +1,236 @@
 import asyncio
+import os
+import subprocess
 import time
+from dataclasses import dataclass
+from typing import Literal, Dict, Optional
+
+import yaml
 
 from qemu.qmp import QMPClient, EventListener
 
-TEST_TIMEOUT_SEC = 3
+from utils import run_cmd_fail
+
+TEST_TIMEOUT_SEC = 1
 SIGNAL_REG = "t1"
 SIGNAL_CONTENT = "de"
+QEMU_EXEC = "/qemu/build/qemu-system-riscv64"
+RV64_ELF = "riscv64-unknown-elf-"
+
+
+@dataclass
+class TestSpec:
+    """Specifies a test case"""
+    id: str
+    asm_core: str
+    reg_tests: {str: str}
+
+
+RegResultType = Dict[str, Dict[Literal['expected', 'actual'], str]]
+
+
+@dataclass
+class TestResult:
+    status: Literal['PASS', 'FAIL']
+    completed_stages: [Literal['COMPILE', 'LINK', 'RUN']]
+    reg_tests: RegResultType
+    errors: [str]
+    duration: str
+
 
 class TestCase:
+    port: int
+    spec: TestSpec
+    qmp: QMPClient
+    listener: EventListener
+    event_handle_task: asyncio.Task
+    test_result: TestResult
+    test_elf: str
 
-  def __init__(self, id: int, test_binary: str):
-    self.id = id
-    self.test_bin = test_binary
-    self.qmp = QMPClient(test_binary)
-    self.listener = EventListener()
-    self.qmp.register_listener(self.listener)
-    self.event_handle_task = asyncio.Task(self._event_handler())
+    def __init__(self, spec: TestSpec, port: int):
+        self.port = port
+        self.spec = spec
+        self.qmp = QMPClient(f"test-{spec.id}")
+        self.listener = EventListener()
+        self.qmp.register_listener(self.listener)
+        self.event_handle_task = asyncio.Task(self._event_handler())
+        reg_tests_result: RegResultType = {str(key): {'expected': str(value), 'actual': 'unknown'} for key, value in
+                                           spec.reg_tests.items()}
+        self.test_result = TestResult(status='PASS', completed_stages=[], reg_tests=reg_tests_result, errors=[],
+                                      duration="0ms")
 
-  async def run(self):
-    await self._start_qemu()
-    await self._connect_qmp()
-    await self.qmp.execute('cont')
-    await self._wait_until_done()
-    print("a0: " + await self._reg_info("a0"))
-    await self._shutdown()
+    async def exec(self):
+        await self._start_qemu()
+        await self._connect_qmp()
+        await self.qmp.execute('cont')
+        await self._wait_until_done()
+        await self._set_results()
+        await self._shutdown()
 
-  async def _event_handler(self):
-    try:
-      async for event in self.listener:
-        # do nothing
-        None
-    except asyncio.CancelledError:
-      return
+    async def compile_and_link(self, arch: str = "rv64i", abi: str = "lp64"):
+        # build assembly source
+        asm_source = self._tmp_file(f"asm-{self.spec.id}.s")
+        _build_assembly_test(self.spec.asm_core, asm_source)
 
-  async def _shutdown(self):
-    await self.qmp.execute('stop')
-    self.event_handle_task.cancel()
-    await self.event_handle_task
-    if self.process.returncode is None:
-      self.process.kill()
-    self.qmp.remove_listener(self.listener)
-    await self.qmp.disconnect()
+        # compile source
+        cmp_out = self._tmp_file(f"cmp-{self.spec.id}.o")
+        await run_cmd_fail(RV64_ELF + "as", f"-march={arch}",
+                           f"-mabi={abi}", "-o", cmp_out, asm_source)
+        self.test_result.completed_stages.append('COMPILE')
 
-  async def _wait_until_done(self):
-    start_time = time.time()
-    while True:
-      poll_time = time.time()
-      sig_reg = await self._reg_info(SIGNAL_REG)
-      if sig_reg.endswith(SIGNAL_CONTENT):
-        break
+        # build the linker script
+        linker_script = self._tmp_file(f"linker-{self.spec.id}.ld")
+        _build_linker_script(cmp_out, linker_script)
 
-      diff_time = poll_time - start_time
-      if diff_time > TEST_TIMEOUT_SEC:
-        raise Exception("Timeout: Test failed due to timeout of finish signal")
+        # link and produce elf
+        self.test_elf = self._tmp_file(f"elf-{self.spec.id}")
+        await run_cmd_fail(RV64_ELF + "ld", "-T",
+                           linker_script, "-o",
+                           self.test_elf, cmp_out)
+        self.test_result.completed_stages.append('LINK')
 
-    end = time.time()
-    print(f"({self.id}, {self.test_bin}) Test took: {(end - start_time) * 1000:.2f}ms", flush=True)
+    async def emit_result(self, dir: str = "results", prefix: str = "result-"):
+        os.makedirs(dir, exist_ok=True)
+        filename = f"{dir}/{prefix}{self.spec.id}.yaml"
+        data = {
+            'id': self.spec.id,
+            'result': self.test_result
+        }
+        with open(filename, 'w') as f:
+            yaml.dump(data, f)
 
-  async def _connect_qmp(self):
-    # TODO: make try counter
-    qmp_port = self._get_qmp_port()
-    excs = []
-    while True:
-      try:
-        await self.qmp.connect(("localhost", qmp_port))
-        break
-      except Exception as e:
-        excs.append(e)
-        None
+    async def _set_results(self):
+        self.test_result.completed_stages.append('RUN')
 
-  async def _start_qemu(self):
-    qmp_addr = f"localhost:{self._get_qmp_port()}"
-    self.process = await asyncio.create_subprocess_exec(
-      "/qemu/build/qemu-system-riscv64",
-      "-nographic",
-      "-S",  # pause on start to wait for debugger
-      "-qmp", f"tcp:{qmp_addr},server=on,wait=off",
-      "-machine", "virt",
-      "-bios", self.test_bin
-    )
+        for reg, val in self.test_result.reg_tests.items():
+            expected = val['expected'].lower().strip()
+            actual = await self._reg_info(reg)
+            val['actual'] = actual
+            if expected != actual:
+                self.test_result.status = 'FAIL'
+                self.test_result.errors.append(f'Reg test {reg} failed: {expected} != {actual}')
 
-  def _get_qmp_port(self) -> int:
-    return 1200 + self.id
+    async def _event_handler(self):
+        try:
+            async for event in self.listener:
+                # do nothing
+                None
+        except asyncio.CancelledError:
+            return
 
-  async def _reg_info(self, reg: str) -> str:
-    response = await self.qmp.execute('human-monitor-command', {'command-line': f'info registers'})
-    return self._extract_register_value(response, reg)
+    async def _shutdown(self):
+        await self.qmp.execute('stop')
+        self.event_handle_task.cancel()
+        await self.event_handle_task
+        if self.process.returncode is None:
+            self.process.kill()
+        self.qmp.remove_listener(self.listener)
+        await self.qmp.disconnect()
 
-  def _extract_register_value(self, registers_info, register_name):
+    async def _wait_until_done(self):
+        start_time = time.time()
+        while True:
+            poll_time = time.time()
+            sig_reg = await self._reg_info(SIGNAL_REG)
+            if sig_reg.endswith(SIGNAL_CONTENT):
+                break
+
+            diff_time = poll_time - start_time
+            if diff_time > TEST_TIMEOUT_SEC:
+                raise Exception(f"Timeout: Test failed due to timeout of finish signal ({diff_time:.4f}s)")
+
+    async def _connect_qmp(self):
+        # TODO: make try counter
+        qmp_port = self._get_qmp_port()
+        first_time = time.time()
+        while True:
+            try:
+                await self.qmp.connect(("localhost", qmp_port))
+                break
+            except Exception as e:
+                if (time.time() - first_time) > TEST_TIMEOUT_SEC:
+                    raise e
+
+    async def _start_qemu(self):
+        qmp_addr = f"localhost:{self._get_qmp_port()}"
+        self.process = await asyncio.create_subprocess_exec(
+            "/qemu/build/qemu-system-riscv64",
+            "-nographic",
+            "-S",  # pause on start to wait for debugger
+            "-qmp", f"tcp:{qmp_addr},server=on,wait=off",
+            "-machine", "virt",
+            "-bios", self.test_elf
+        )
+
+    def _get_qmp_port(self) -> int:
+        return self.port
+
+    async def _reg_info(self, reg: str) -> str:
+        response = await self.qmp.execute('human-monitor-command', {'command-line': f'info registers'})
+        return _extract_register_value(response, reg)
+
+    def _tmp_file(self, name: str) -> str:
+        build_dir = f"/tmp/build-{self.spec.id}/"
+        os.makedirs(build_dir, exist_ok=True)
+        return f"{build_dir}/{name}"
+
+
+def _build_linker_script(obj_file: str, out_path: str) -> str:
+    # set our test assembly at 0x80000000
+    content = f"""
+    MEMORY {{
+      dram_space (rwx) : ORIGIN = 0x80000000, LENGTH = 0x8000000
+    }}
+    
+    SECTIONS {{
+      .text : {{
+        {obj_file}(.text.test)
+      }} > dram_space
+    }}
+    """
+
+    with open(out_path, "w") as f:
+        f.write(content)
+
+
+def _build_assembly_test(core: str, out_path: str) -> str:
+    if "t1" in core or "x6" in core:
+        raise Exception(
+            "Failed to build assembly: You cannot use the t1 register, as it is used as internal signal register!")
+
+    content = f"""
+    .global _start
+    .section .text.test
+    
+    _start:	
+      {core}
+    
+    # the qmp script polls this t1 to check whether the test has ended
+    signal_stop:
+      addi t1, x0, 0xde
+    
+    loop:	j loop
+    """
+
+    with open(out_path, "w") as f:
+        f.write(content)
+
+
+def _extract_register_value(registers_info: str, register_name: str) -> Optional[str]:
     # Split the registers_info into lines
     lines = registers_info.split('\n')
 
     # Iterate through each line
     for line in lines:
-      # Split the line into register name-value pairs
-      parts = line.split()
+        # Split the line into register name-value pairs
+        parts = line.split()
 
-      # Iterate through each register name-value pair
-      for i in range(0, len(parts), 2):
-        # Check if the current part matches the register name
-        if register_name in parts[i].strip():
-          # Return the corresponding value without formatting
-          return parts[i + 1]
+        # Iterate through each register name-value pair
+        for i in range(0, len(parts), 2):
+            # Check if the current part matches the register name
+            if register_name in parts[i].strip():
+                # Return the corresponding value without formatting
+                return parts[i + 1]
 
     # If register name is not found, return None
     return None
