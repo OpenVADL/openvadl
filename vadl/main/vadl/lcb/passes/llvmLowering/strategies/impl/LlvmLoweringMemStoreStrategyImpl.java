4d2ec8bbc4dd791d722e2e922c6030d6fe287153
Kevin Per <kevin.per@protonmail.com>
2024-08-21 13:45:39 +0200

    lcb: Setup strategy

---
+++ vadl/main/vadl/lcb/passes/llvmLowering/strategies/impl/LlvmLoweringMemStoreStrategyImpl.java
@@ -0,0 +1,31 @@
+package vadl.lcb.passes.llvmLowering.strategies.impl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import vadl.lcb.passes.isaMatching.InstructionLabel;
+import vadl.lcb.passes.llvmLowering.strategies.LlvmLoweringStrategy;
+import vadl.lcb.passes.llvmLowering.tablegen.model.TableGenInstructionOperand;
+import vadl.lcb.passes.llvmLowering.tablegen.model.TableGenPattern;
+import vadl.viam.Instruction;
+import vadl.viam.passes.functionInliner.UninlinedGraph;
+
+/**
+ * Lowers instructions which can store into memory.
+ */
+public class LlvmLoweringMemStoreStrategyImpl extends LlvmLoweringStrategy {
+  @Override
+  protected Set<InstructionLabel> getSupportedInstructionLabels() {
+    return Set.of(InstructionLabel.STORE_MEM);
+  }
+
+  @Override
+  protected List<TableGenPattern> generatePatternVariations(
+      Map<InstructionLabel, List<Instruction>> supportedInstructions,
+      InstructionLabel instructionLabel, UninlinedGraph behavior,
+      List<TableGenInstructionOperand> inputOperands,
+      List<TableGenInstructionOperand> outputOperands, List<TableGenPattern> patterns) {
+    return Collections.emptyList();
+  }
+}
