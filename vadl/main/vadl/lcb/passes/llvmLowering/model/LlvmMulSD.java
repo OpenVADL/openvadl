00eee0c690af731deeb17020ebc6870a21336cff
Kevin Per <kevin.per@protonmail.com>
2024-08-22 22:15:30 +0200

    lcb: Added missing methods to Llvm's selection dag nodes

--- vadl/main/vadl/lcb/passes/llvmLowering/model/LlvmMulSD.java
+++ vadl/main/vadl/lcb/passes/llvmLowering/model/LlvmMulSD.java
@@ -1,8 +1,12 @@
 package vadl.lcb.passes.llvmLowering.model;
 
 import vadl.lcb.passes.llvmLowering.LlvmNodeLowerable;
+import vadl.lcb.passes.llvmLowering.strategies.visitors.TableGenMachineInstructionVisitor;
+import vadl.lcb.passes.llvmLowering.strategies.visitors.TableGenNodeVisitor;
 import vadl.types.BuiltInTable;
 import vadl.types.Type;
+import vadl.viam.graph.GraphNodeVisitor;
+import vadl.viam.graph.Node;
 import vadl.viam.graph.NodeList;
 import vadl.viam.graph.dependency.BuiltInCall;
 import vadl.viam.graph.dependency.ExpressionNode;
@@ -20,5 +24,28 @@ public class LlvmMulSD extends BuiltInCall implements LlvmNodeLowerable {
   @Override
   public String lower() {
     return "mul";
+  }
+
+
+  @Override
+  public void accept(GraphNodeVisitor visitor) {
+    if (visitor instanceof TableGenMachineInstructionVisitor v) {
+      v.visit(this);
+    } else if (visitor instanceof TableGenNodeVisitor v) {
+      v.visit(this);
+    } else {
+      visitor.visit(this);
+    }
+  }
+
+  @Override
+  public Node copy() {
+    return new LlvmMulSD(new NodeList<>(args.stream().map(x -> (ExpressionNode) x.copy()).toList()),
+        type());
+  }
+
+  @Override
+  public Node shallowCopy() {
+    return new LlvmMulSD(args, type());
   }
 }
