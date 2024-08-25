95b32ac14ff7d553a73004ef488694d5b0daa7b8
Kevin Per <kevin.per@protonmail.com>
2024-08-25 21:03:55 +0200

    lcb: Defined calling conventions

--- vadl/main/vadl/viam/passes/dummyAbi/DummyAbi.java
+++ vadl/main/vadl/viam/passes/dummyAbi/DummyAbi.java
@@ -1,5 +1,8 @@
 package vadl.viam.passes.dummyAbi;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import vadl.viam.Definition;
 import vadl.viam.DefinitionVisitor;
 import vadl.viam.Identifier;
@@ -11,6 +14,7 @@ import vadl.viam.RegisterFile;
 public class DummyAbi extends Definition {
 
   enum Alignment {
+    NO_ALIGNMENT(-1),
     HALF_WORD(4),
     WORD(8);
 
@@ -21,15 +25,29 @@ public class DummyAbi extends Definition {
     }
   }
 
-  record RegisterRef(RegisterFile registerFile,
+  public record RegisterRef(RegisterFile registerFile,
                      int addr,
                      Alignment alignment) {
-
+    public String render() {
+      return registerFile.identifier.simpleName() + addr;
+    }
   }
+
+  record RegisterAlias(String value) {
+  }
+
+
 
   private final RegisterRef returnAddress;
   private final RegisterRef stackPointer;
   private final RegisterRef framePointer;
+
+
+  private final Map<RegisterRef, RegisterAlias> aliases;
+  private final List<RegisterRef> callerSaved;
+  private final List<RegisterRef> calleeSaved;
+  private final List<RegisterRef> argumentRegisters;
+  private final List<RegisterRef> returnRegisters;
 
   /**
    * Constructor.
@@ -37,16 +55,59 @@ public class DummyAbi extends Definition {
   public DummyAbi(Identifier identifier,
                   RegisterRef returnAddress,
                   RegisterRef stackPointer,
-                  RegisterRef framePointer
+                  RegisterRef framePointer,
+                  Map<RegisterRef, RegisterAlias> aliases,
+                  List<RegisterRef> callerSaved,
+                  List<RegisterRef> calleeSaved,
+                  List<RegisterRef> argumentRegisters,
+                  List<RegisterRef> returnRegisters
   ) {
     super(identifier);
     this.returnAddress = returnAddress;
     this.stackPointer = stackPointer;
     this.framePointer = framePointer;
+    this.aliases = aliases;
+    this.callerSaved = callerSaved;
+    this.calleeSaved = calleeSaved;
+    this.argumentRegisters = argumentRegisters;
+    this.returnRegisters = returnRegisters;
   }
 
   @Override
   public void accept(DefinitionVisitor visitor) {
     visitor.visit(this);
+  }
+
+
+  public RegisterRef returnAddress() {
+    return returnAddress;
+  }
+
+  public RegisterRef stackPointer() {
+    return stackPointer;
+  }
+
+  public RegisterRef framePointer() {
+    return framePointer;
+  }
+
+  public Map<RegisterRef, RegisterAlias> aliases() {
+    return aliases;
+  }
+
+  public List<RegisterRef> callerSaved() {
+    return callerSaved;
+  }
+
+  public List<RegisterRef> calleeSaved() {
+    return calleeSaved;
+  }
+
+  public List<RegisterRef> argumentRegisters() {
+    return argumentRegisters;
+  }
+
+  public List<RegisterRef> returnRegisters() {
+    return returnRegisters;
   }
 }
--- vadl/main/vadl/viam/passes/dummyAbi/DummyAbiPass.java
+++ vadl/main/vadl/viam/passes/dummyAbi/DummyAbiPass.java
@@ -1,6 +1,8 @@
 package vadl.viam.passes.dummyAbi;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.Nullable;
 import vadl.configuration.GeneralConfiguration;
 import vadl.pass.Pass;
@@ -8,6 +10,7 @@ import vadl.pass.PassName;
 import vadl.pass.PassResults;
 import vadl.utils.SourceLocation;
 import vadl.viam.Identifier;
+import vadl.viam.RegisterFile;
 import vadl.viam.Specification;
 
 /**
@@ -29,11 +32,98 @@ public class DummyAbiPass extends Pass {
   public Object execute(PassResults passResults, Specification viam)
       throws IOException {
     var registerFile = viam.registerFiles().findFirst().get();
+    var callerSaved = getCallerSaved(registerFile);
+    var calleeSaved = getCalleeSaved(registerFile);
+    var aliases = getAliases(registerFile);
+    var argumentRegisters = getArgumentRegisters(registerFile);
+    var returnRegisters = getReturnRegisters(registerFile);
+
     viam.add(new DummyAbi(new Identifier("dummyAbi", SourceLocation.INVALID_SOURCE_LOCATION),
         new DummyAbi.RegisterRef(registerFile, 1, DummyAbi.Alignment.WORD),
         new DummyAbi.RegisterRef(registerFile, 2, DummyAbi.Alignment.HALF_WORD),
-        new DummyAbi.RegisterRef(registerFile, 8, DummyAbi.Alignment.WORD)));
+        new DummyAbi.RegisterRef(registerFile, 8, DummyAbi.Alignment.WORD),
+        aliases,
+        callerSaved,
+        calleeSaved,
+        argumentRegisters,
+        returnRegisters));
 
     return null;
+  }
+
+  private List<DummyAbi.RegisterRef> getReturnRegisters(RegisterFile registerFile) {
+    return List.of(
+        new DummyAbi.RegisterRef(registerFile, 10, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 11, DummyAbi.Alignment.NO_ALIGNMENT)
+    );
+  }
+
+  private List<DummyAbi.RegisterRef> getArgumentRegisters(RegisterFile registerFile) {
+    return List.of(
+        new DummyAbi.RegisterRef(registerFile, 10, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 11, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 12, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 13, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 14, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 15, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 16, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 17, DummyAbi.Alignment.NO_ALIGNMENT)
+    );
+  }
+
+  private Map<DummyAbi.RegisterRef, DummyAbi.RegisterAlias> getAliases(RegisterFile registerFile) {
+    return Map.of(
+        new DummyAbi.RegisterRef(registerFile, 0, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("zero"),
+        new DummyAbi.RegisterRef(registerFile, 1, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("ra"),
+        new DummyAbi.RegisterRef(registerFile, 2, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("sp"),
+        new DummyAbi.RegisterRef(registerFile, 3, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("gp"),
+        new DummyAbi.RegisterRef(registerFile, 4, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("tp"),
+        new DummyAbi.RegisterRef(registerFile, 8, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterAlias("fp")
+    );
+  }
+
+  private List<DummyAbi.RegisterRef> getCalleeSaved(RegisterFile registerFile) {
+    return List.of(
+        new DummyAbi.RegisterRef(registerFile, 2, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 8, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 9, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 18, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 19, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 20, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 21, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 22, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 23, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 24, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 25, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 26, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 27, DummyAbi.Alignment.NO_ALIGNMENT)
+    );
+  }
+
+  private List<DummyAbi.RegisterRef> getCallerSaved(RegisterFile registerFile) {
+    return List.of(
+        new DummyAbi.RegisterRef(registerFile, 1, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 10, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 11, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 12, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 13, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 14, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 15, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 16, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 17, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 5, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 6, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 7, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 28, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 29, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 30, DummyAbi.Alignment.NO_ALIGNMENT),
+        new DummyAbi.RegisterRef(registerFile, 31, DummyAbi.Alignment.NO_ALIGNMENT)
+    );
   }
 }
