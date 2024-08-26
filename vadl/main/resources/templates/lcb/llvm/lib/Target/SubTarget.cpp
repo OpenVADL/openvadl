f40e35c9e9df88491eb52712acb6facaa7fe8ba9
Kevin Per <kevin.per@protonmail.com>
2024-08-26 15:50:53 +0200

    lcb: Define subtarget

--- vadl/main/resources/templates/lcb/llvm/lib/Target/SubTarget.cpp
+++ vadl/main/resources/templates/lcb/llvm/lib/Target/SubTarget.cpp
@@ -1,25 +1,25 @@
-#include "«processorName»Subtarget.h"
+#include "[(${namespace})]Subtarget.h"
 #include "llvm/Support/Debug.h"
 
-#define DEBUG_TYPE "«processorName»Subtarget"
+#define DEBUG_TYPE "[(${namespace})]Subtarget"
 
 using namespace llvm;
 
 #define GET_SUBTARGETINFO_TARGET_DESC
 #define GET_SUBTARGETINFO_CTOR
-#include "«processorName»GenSubtargetInfo.inc"
+#include "[(${namespace})]GenSubtargetInfo.inc"
 
-void «processorName»Subtarget::anchor() {}
+void [(${namespace})]Subtarget::anchor() {}
 
-«processorName»Subtarget::«processorName»Subtarget(const Triple &TargetTriple, StringRef Cpu, StringRef TuneCPU, StringRef FeatureString, const TargetMachine &TM, const TargetOptions &Options, CodeModel::Model CodeModel, CodeGenOpt::Level OptLevel)
-    : «processorName»GenSubtargetInfo(TargetTriple, Cpu, TuneCPU, FeatureString), InstrInfo(*this) // «processorName»InstrInfo
+[(${namespace})]Subtarget::[(${namespace})]Subtarget(const Triple &TargetTriple, StringRef Cpu, StringRef TuneCPU, StringRef FeatureString, const TargetMachine &TM, const TargetOptions &Options, CodeModel::Model CodeModel, CodeGenOpt::Level OptLevel)
+    : [(${namespace})]GenSubtargetInfo(TargetTriple, Cpu, TuneCPU, FeatureString), InstrInfo(*this) // [(${namespace})]InstrInfo
       ,
-      FrameLowering(*this) // «processorName»FrameLowering
+      FrameLowering(*this) // [(${namespace})]FrameLowering
       ,
-      TLInfo(TM, *this) // «processorName»TargetLowering
+      TLInfo(TM, *this) // [(${namespace})]TargetLowering
       ,
       TSInfo() // SelectionDAGTargetInfo
       ,
-      RegInfo() // «processorName»RegisterInfo
+      RegInfo() // [(${namespace})]RegisterInfo
 {
 }
