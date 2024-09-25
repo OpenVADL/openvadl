eb05b1744c081992e7450444f6a2e89916287725
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-09-25 09:41:45 +0200

    iss: Add not activated machine.c implementation

---
+++ vadl/main/resources/templates/iss/target/gen-arch/machine.c
@@ -0,0 +1,27 @@
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "qemu/error-report.h"
+#include "sysemu/kvm.h"
+#include "migration/cpu.h"
+#include "sysemu/cpu-timers.h"
+
+
+// TODO: Activate/Use in cpu.h#cpu_class_init at DeviceClass#vmsd (dc->vmsd=&vms_..._cpu)
+// The VMState definition that is used to save and restore
+// the state of the CPU device.
+const VMStateDescription vms_[(${gen_arch_lower})]_cpu = {
+    .name = "cpu",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .fields = (VMStateField[]) {
+      [# th:each="reg_file, iterState : ${register_files}"] // CPU register file(s)
+        VMSTATE_UINTTL_ARRAY(env.[(${reg_file.name_lower})], [(${gen_arch_upper})]CPU, [(${reg_file["size"]})]),
+      [/]
+      [# th:each="reg, iterState : ${registers}"] // CPU registers
+        VMSTATE_UINTTL(env.[(${reg.name_lower})], [(${gen_arch_upper})]CPU),
+      [/]
+
+        VMSTATE_END_OF_LIST()
+    }
+};
---
+++ vadl/main/vadl/iss/template/target/EmitIssMachinePass.java
@@ -0,0 +1,30 @@
+package vadl.iss.template.target;
+
+import java.util.Map;
+import vadl.configuration.IssConfiguration;
+import vadl.iss.template.IssTemplateRenderingPass;
+import vadl.pass.PassResults;
+import vadl.viam.Specification;
+
+/**
+ * Emits the target/gen-arch/translate.c that contains the functions to generate
+ * the TCG instructions from decoded guest instructions.
+ * It also contains the {@code gen_intermediate_code} function, called by QEMU as
+ * entry point to start the TCG generation.
+ */
+public class EmitIssMachinePass extends IssTemplateRenderingPass {
+  public EmitIssMachinePass(IssConfiguration configuration) {
+    super(configuration);
+  }
+
+  @Override
+  protected String issTemplatePath() {
+    return "target/gen-arch/machine.c";
+  }
+
+  @Override
+  protected Map<String, Object> createVariables(PassResults passResults,
+                                                Specification specification) {
+    return super.createVariables(passResults, specification);
+  }
+}
