11087478a529cac3044408f4ec00909d2611a08e
Kevin Per <kevin.per@protonmail.com>
2024-10-02 08:17:15 +0000

    lcb: Added test which checks if registers exist

--- vadl-test/main/vadl/test/lcb/passes/GenerateRegisterClassesPassTest.java
+++ vadl-test/main/vadl/test/lcb/passes/GenerateRegisterClassesPassTest.java
@@ -14,7 +14,7 @@ public class GenerateRegisterClassesPassTest extends AbstractLcbTest {
   @ParameterizedTest
   @CsvSource({"processorNameValue,X,32,i64",
       "processorNameValue,X0Class,32,i64",
-  "processorNameValue,X1Class,32,i64",
+      "processorNameValue,X1Class,32,i64",
       "processorNameValue,X2Class,32,i64",
       "processorNameValue,X3Class,32,i64",
       "processorNameValue,X4Class,32,i64",
@@ -66,5 +66,92 @@ public class GenerateRegisterClassesPassTest extends AbstractLcbTest {
     Assertions.assertEquals(name, rg.get().name());
     Assertions.assertEquals(alignment, rg.get().alignment());
     Assertions.assertEquals(type, rg.get().regTypes().get(0).getLlvmType());
+  }
+
+
+  @ParameterizedTest
+  @CsvSource({"processorNameValue,X,X0",
+      "processorNameValue,X,X1",
+      "processorNameValue,X,X2",
+      "processorNameValue,X,X3",
+      "processorNameValue,X,X4",
+      "processorNameValue,X,X5",
+      "processorNameValue,X,X6",
+      "processorNameValue,X,X7",
+      "processorNameValue,X,X8",
+      "processorNameValue,X,X9",
+      "processorNameValue,X,X10",
+      "processorNameValue,X,X11",
+      "processorNameValue,X,X12",
+      "processorNameValue,X,X13",
+      "processorNameValue,X,X14",
+      "processorNameValue,X,X15",
+      "processorNameValue,X,X16",
+      "processorNameValue,X,X17",
+      "processorNameValue,X,X18",
+      "processorNameValue,X,X19",
+      "processorNameValue,X,X20",
+      "processorNameValue,X,X21",
+      "processorNameValue,X,X22",
+      "processorNameValue,X,X23",
+      "processorNameValue,X,X24",
+      "processorNameValue,X,X25",
+      "processorNameValue,X,X26",
+      "processorNameValue,X,X27",
+      "processorNameValue,X,X28",
+      "processorNameValue,X,X29",
+      "processorNameValue,X,X30",
+      "processorNameValue,X,X31",
+      "processorNameValue,X0Class,X0",
+      "processorNameValue,X1Class,X1",
+      "processorNameValue,X2Class,X2",
+      "processorNameValue,X3Class,X3",
+      "processorNameValue,X4Class,X4",
+      "processorNameValue,X5Class,X5",
+      "processorNameValue,X6Class,X6",
+      "processorNameValue,X7Class,X7",
+      "processorNameValue,X8Class,X8",
+      "processorNameValue,X9Class,X9",
+      "processorNameValue,X10Class,X10",
+      "processorNameValue,X11Class,X11",
+      "processorNameValue,X12Class,X12",
+      "processorNameValue,X13Class,X13",
+      "processorNameValue,X14Class,X14",
+      "processorNameValue,X15Class,X15",
+      "processorNameValue,X16Class,X16",
+      "processorNameValue,X17Class,X17",
+      "processorNameValue,X18Class,X18",
+      "processorNameValue,X19Class,X19",
+      "processorNameValue,X20Class,X20",
+      "processorNameValue,X21Class,X21",
+      "processorNameValue,X22Class,X22",
+      "processorNameValue,X23Class,X23",
+      "processorNameValue,X24Class,X24",
+      "processorNameValue,X25Class,X25",
+      "processorNameValue,X26Class,X26",
+      "processorNameValue,X27Class,X27",
+      "processorNameValue,X28Class,X28",
+      "processorNameValue,X29Class,X29",
+      "processorNameValue,X30Class,X30",
+      "processorNameValue,X31Class,X31"
+  })
+  void shouldHaveCorrectRegisterAssignments(String namespace, String name, String reg)
+      throws IOException, DuplicatedPassKeyException {
+    // Given
+    var setup = runLcb(getConfiguration(false), "sys/risc-v/rv64im.vadl",
+        new PassKey(GenerateRegisterClassesPass.class.getName()));
+    var passManager = setup.passManager();
+    var spec = setup.specification();
+
+    // When
+    var generatedRegisterClasses = (GenerateRegisterClassesPass.Output) passManager.getPassResults()
+        .lastResultOf(GenerateRegisterClassesPass.class);
+
+    // Then
+    var rg = generatedRegisterClasses.registerClasses().stream().filter(x -> x.name().equals(name))
+        .findFirst();
+    Assertions.assertTrue(rg.isPresent());
+    var x = rg.get().registers().stream().filter(y -> y.registerName().equals(reg)).findFirst();
+    Assertions.assertTrue(x.isPresent(), "Register was not found");
   }
 }
