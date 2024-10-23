5538d57bceef3d0db0785d0cfe61c90693b35efa
Kevin Per <kevin.per@protonmail.com>
2024-10-23 10:03:27 +0000

    lcb: Implemented format modifier

--- vadl/main/resources/templates/lcb/llvm/lib/Target/MCTargetDesc/AsmUtils.cpp
+++ vadl/main/resources/templates/lcb/llvm/lib/Target/MCTargetDesc/AsmUtils.cpp
@@ -10,8 +10,6 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <bitset>
-#include <iomanip>
 
 #define DEBUG_TYPE "AsmUtils"
 
@@ -224,6 +222,12 @@ std::string AsmUtils::formatExpr(const MCExpr *Expr, uint8_t Radix, const MCAsmI
 
 std::string AsmUtils::FormatModifier(const [(${namespace})]MCExpr::VariantKind VariantKind)
 {
+    [# th:each="fm : ${formatModifiers}" ]
+    if(VariantKind == [(${namespace})]MCExpr::VariantKind::[(${fm.variantKind().value()})]) {
+      return "[(${fm.relocation().identifier.simpleName()})]";
+    }
+    [/]
+
     return "unknown";
 }
 
--- vadl/main/vadl/lcb/template/lib/Target/MCTargetDesc/EmitAsmUtilsCppFilePass.java
+++ vadl/main/vadl/lcb/template/lib/Target/MCTargetDesc/EmitAsmUtilsCppFilePass.java
@@ -1,8 +1,12 @@
 package vadl.lcb.template.lib.Target.MCTargetDesc;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import vadl.configuration.LcbConfiguration;
+import vadl.gcb.passes.relocation.model.ConcreteLogicalRelocation;
+import vadl.gcb.passes.relocation.model.LogicalRelocation;
+import vadl.lcb.passes.relocation.GenerateLinkerComponentsPass;
 import vadl.lcb.template.CommonVarNames;
 import vadl.lcb.template.LcbTemplateRenderingPass;
 import vadl.lcb.templateUtils.RegisterUtils;
@@ -30,6 +34,17 @@ public class EmitAsmUtilsCppFilePass extends LcbTemplateRenderingPass {
     return "llvm/lib/Target/" + processorName + "/MCTargetDesc/AsmUtils.cpp";
   }
 
+  private List<ConcreteLogicalRelocation> formatModifier(PassResults passResults) {
+    var container = (GenerateLinkerComponentsPass.Output) passResults.lastResultOf(
+        GenerateLinkerComponentsPass.class);
+    return container.elfRelocations()
+        .stream()
+        .filter(x -> x instanceof ConcreteLogicalRelocation)
+        .map(x -> (ConcreteLogicalRelocation) x)
+        .filter(distinctByKey(LogicalRelocation::variantKind))
+        .toList();
+  }
+
   @Override
   protected Map<String, Object> createVariables(final PassResults passResults,
                                                 Specification specification) {
@@ -38,7 +53,8 @@ public class EmitAsmUtilsCppFilePass extends LcbTemplateRenderingPass {
         specification.registerFiles().map(RegisterUtils::getRegisterClass)
             .flatMap(x -> x.registers().stream()).toList(),
         "registerClasses",
-        specification.registerFiles().map(RegisterUtils::getRegisterClass).toList()
+        specification.registerFiles().map(RegisterUtils::getRegisterClass).toList(),
+        "formatModifiers", formatModifier(passResults)
     );
   }
 }
