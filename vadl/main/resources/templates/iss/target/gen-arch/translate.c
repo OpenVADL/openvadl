ec8ab9e51d4805c9c9bdf6dc5adab657d5b5c4a4
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-10-29 13:17:12 +0100

    iss: Fix gen_trunc helper function

--- vadl/main/resources/templates/iss/target/gen-arch/translate.c
+++ vadl/main/resources/templates/iss/target/gen-arch/translate.c
@@ -136,7 +136,7 @@ static inline void gen_trunc(TCGv dest, TCGv arg, int bitWidth) {
     if (bitWidth == 32) {
         tcg_gen_extrl_i64_i32((TCGv_i32) dest, arg);
     } else {
-        tcg_gen_andi_tl(dest, arg, (bitWidth << 1) - 1);
+        tcg_gen_andi_tl(dest, arg, (1ULL << bitWidth) - 1);
     }
 }
 
