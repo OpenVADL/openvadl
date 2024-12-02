ce213c7d22da6e397263d47a2579983218af6812
Kevin Per <kevin.per@protonmail.com>
2024-12-02 14:50:36 +0000

    Revert "Squashed 'vadl-test/main/resources/embench/' content from commit cf7fbd72"
    This reverts commit 02a3d19d309b318c61d64387a6536b038c02ff52.

--- __run_spike.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-OUTPUT=$((time -p spike --isa=rv32ima $1 ) 2>&1)
-RET=$?
-echo $OUTPUT | sed -nE 's/real[[:space:]]*([0-9]+)\.([0-9]+).*/TIME=\1.\2/p'
-
-exit $RET
--- run_in_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-image=$1
-shift
-cmd=$@
-
-docker run --rm -v .:/embench $image sh -c "cd /embench; $cmd"
