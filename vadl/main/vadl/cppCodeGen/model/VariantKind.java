5513940ae0952fdbb2076046aacc1719dc4c1c73
Kevin Per <kevin.per@protonmail.com>
2024-12-07 20:27:35 +0000

    lcb: Fixed immediate pseudo instruction expansion

--- vadl/main/vadl/cppCodeGen/model/VariantKind.java
+++ vadl/main/vadl/cppCodeGen/model/VariantKind.java
@@ -7,30 +7,30 @@ import vadl.viam.Relocation;
 /**
  * Kind for {@link LogicalRelocation} and immediates.
  */
-public record VariantKind(String value, String human) {
+public record VariantKind(String value, String human, boolean isImmediate) {
   public VariantKind(Format.Field field) {
-    this("VK_" + field.identifier.lower(), field.identifier.lower());
+    this("VK_" + field.identifier.lower(), field.identifier.lower(), true);
   }
 
   public VariantKind(Relocation relocation) {
-    this("VK_" + relocation.identifier.lower(), relocation.identifier.simpleName());
+    this("VK_" + relocation.identifier.lower(), relocation.identifier.simpleName(), false);
   }
 
   public static VariantKind none() {
-    return new VariantKind("VK_None", "None");
+    return new VariantKind("VK_None", "None", false);
   }
 
   public static VariantKind invalid() {
-    return new VariantKind("VK_Invalid", "Invalid");
+    return new VariantKind("VK_Invalid", "Invalid", false);
   }
 
   public static VariantKind absolute(Format.Field field) {
     return new VariantKind("VK_SYMB_ABS_" + field.identifier.lower(),
-        "SYMB_ABS_" + field.identifier.lower());
+        "SYMB_ABS_" + field.identifier.lower(), false);
   }
 
   public static VariantKind relative(Format.Field field) {
     return new VariantKind("VK_SYMB_PCREL_" + field.identifier.lower(),
-        "SYMB_PCREL_" + field.identifier.lower());
+        "SYMB_PCREL_" + field.identifier.lower(), false);
   }
 }
