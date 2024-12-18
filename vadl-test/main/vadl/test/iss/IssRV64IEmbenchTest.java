a67bc51297aaeeb808a65e0c2efcc04addb5de59
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-12-18 22:35:42 +0100

    iss-testing: Add embench test

---
+++ vadl-test/main/vadl/test/iss/IssRV64IEmbenchTest.java
@@ -0,0 +1,37 @@
+package vadl.test.iss;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.utility.MountableFile;
+import vadl.pass.exception.DuplicatedPassKeyException;
+import vadl.utils.VadlFileUtils;
+
+public class IssRV64IEmbenchTest extends QemuIssTest {
+
+  private static final Logger log = LoggerFactory.getLogger(IssRV64IEmbenchTest.class);
+
+  // TODO: Remove this (it is just for testing purposes)
+  @Test
+  void issLoweringTest() throws IOException, DuplicatedPassKeyException {
+    var image = generateSimulator("sys/risc-v/rv64i.vadl");
+
+    // load embench from resources
+    var embenchPath = VadlFileUtils.copyResourceDirToTempDir("embench", "embench");
+
+    // build benchmarks for rv64i spike and run benchmarks in generated iss
+    var runCommand = "chmod -R +x /work/embench " +
+        "&& cd /work/embench " +
+        "&& bash ./build_spike-rv64i.sh " +
+        "&& bash ./benchmark_qemu.sh qemu-system-vadl -nographic -M virt -bios";
+
+    runContainer(image,
+        container -> container
+            .withCopyFileToContainer(MountableFile.forHostPath(embenchPath), "/work/embench")
+            .withCommand("/bin/bash", "-c", runCommand),
+        null);
+  }
+}
+
