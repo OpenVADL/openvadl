1e08d6c6ec72e82b948c3cd2f8fd06ae6df02b7d
Kevin Per <kevin.per@protonmail.com>
2024-12-21 12:03:23 +0000

    lcb: Handle special spike exit code

--- vadl-test/main/resources/images/spike_rv32im/base_image/lcb_wrapper.sh
+++ vadl-test/main/resources/images/spike_rv32im/base_image/lcb_wrapper.sh
@@ -10,4 +10,6 @@ set -x
 /opt/riscv-cross/bin/riscv32-unknown-linux-gnu-gcc -static -nostartfiles -T/helper/link_lcbw.ld /tmp/main.o /helper/init.o /helper/trap.o /helper/vars.spike.o /helper/common.o -o /tmp/main
 
 echo "Running spike..."
-timeout --preserve-status 5 /opt/spike/bin/spike --isa=${SPIKE_TARGET} /tmp/main
+# We need the `grep` because spike does return exit `0` in some cases
+# where we actually would need a exit `1`.
+timeout --preserve-status 5 /opt/spike/bin/spike --isa=${SPIKE_TARGET} /tmp/main | grep -vzq "FAILED"
