c07a0b69e3970a59a3344d11f6d5c3f9c257cb0f
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-12-22 12:00:35 +0100

    iss: Make target size dynamic

--- vadl/main/resources/templates/iss/target/gen-arch/cpu-param.h
+++ vadl/main/resources/templates/iss/target/gen-arch/cpu-param.h
@@ -2,8 +2,9 @@
 #ifndef [(${gen_arch_upper})]_CPU_PARAM_H
 #define [(${gen_arch_upper})]_CPU_PARAM_H
 
+#define TARGET_LONG_BITS [(${target_size})]
+
 // TODO: replace this by correct values
-#define TARGET_LONG_BITS 64
 #define TARGET_PAGE_BITS 12
 
 #define TARGET_PHYS_ADDR_SPACE_BITS 64
