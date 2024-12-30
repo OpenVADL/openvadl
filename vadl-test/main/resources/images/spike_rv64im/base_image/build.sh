b39bdd74b5b6570521aa8b09e7c25c34a16645a4
Kevin Per <kevin.per@protonmail.com>
2024-12-30 11:29:44 +0000

    lcb: Fixed riscv64 spike

--- vadl-test/main/resources/images/spike_rv64im/base_image/build.sh
+++ vadl-test/main/resources/images/spike_rv64im/base_image/build.sh
@@ -1,1 +1,2 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t kper1337/riscv64-spike-lcb-test:v3 -f Dockerfile . --push
+#docker buildx build --platform linux/amd64,linux/arm64 -t kper1337/riscv64-spike-lcb-test:v3 -f Dockerfile . --push
+docker buildx build --platform linux/amd64 -t kper1337/riscv64-spike-lcb-test:v3 -f Dockerfile . --push
