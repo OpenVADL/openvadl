afed3ae5e1bdcf7dcf03940fe79087feb816996b
Kevin Per <kevin.per@protonmail.com>
2024-12-30 14:05:56 +0000

    lcb: Updated assertions

--- vadl-test/main/resources/llvm/riscv/assertions/rv64im/large_constant_with_compare.c.s
+++ vadl-test/main/resources/llvm/riscv/assertions/rv64im/large_constant_with_compare.c.s
@@ -4,22 +4,24 @@
 	.type	main,@function
 main:                                   # @main
 # %bb.0:                                # %entry
-	ADDI sp,sp,-16
-	SD fp,8(sp)
-	ADDI fp,sp,16
-	ADDI ra,zero,0
-	SW ra,-12(fp)
-	ADDI ra,zero,1
-	SW ra,-16(fp)
-	LWU ra,-16(fp)
-	XORI ra,ra,1
-	SLTU a0,zero,ra
-	LD fp,8(sp)
-	ADDI sp,sp,16
+	ADDI sp,sp,-32
+	SD fp,24(sp)
+	SD ra,16(sp)
+	ADDI fp,sp,32
+	ADDI a0,zero,0
+	SW a0,-20(fp)
+	ADDI a0,zero,1
+	SW a0,-24(fp)
+	LWU a0,-24(fp)
+	XORI a0,a0,1
+	SLTU a0,zero,a0
+	LD ra,16(sp)
+	LD fp,24(sp)
+	ADDI sp,sp,32
 	JALR zero,0(ra)
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
                                         # -- End function
-	.ident	"clang version 17.0.6 (https://github.com/llvm/llvm-project.git 6009708b4367171ccdbf4b5905cb6a803753fe18)"
+	.ident	"clang version 17.0.6 (https://github.com/llvm/llvm-project 6009708b4367171ccdbf4b5905cb6a803753fe18)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
--- vadl-test/main/resources/llvm/riscv/assertions/rv64im/large_negative_constant.c.s
+++ vadl-test/main/resources/llvm/riscv/assertions/rv64im/large_negative_constant.c.s
@@ -4,20 +4,22 @@
 	.type	constant_return,@function
 constant_return:                        # @constant_return
 # %bb.0:                                # %entry
-	ADDI sp,sp,-16
-	SD fp,8(sp)
-	ADDI fp,sp,16
-                                        # kill: def $x1 killed $x10
-	SW a0,-12(fp)
-	LW ra,-12(fp)
-	LUI tp,0x80
-	ADD a0,ra,tp
-	LD fp,8(sp)
-	ADDI sp,sp,16
+	ADDI sp,sp,-32
+	SD fp,24(sp)
+	SD ra,16(sp)
+	ADDI fp,sp,32
+                                        # kill: def $x11 killed $x10
+	SW a0,-20(fp)
+	LW a0,-20(fp)
+	LUI a1,0x80000
+	ADD a0,a0,a1
+	LD ra,16(sp)
+	LD fp,24(sp)
+	ADDI sp,sp,32
 	JALR zero,0(ra)
 .Lfunc_end0:
 	.size	constant_return, .Lfunc_end0-constant_return
                                         # -- End function
-	.ident	"clang version 17.0.6 (https://github.com/llvm/llvm-project.git 6009708b4367171ccdbf4b5905cb6a803753fe18)"
+	.ident	"clang version 17.0.6 (https://github.com/llvm/llvm-project 6009708b4367171ccdbf4b5905cb6a803753fe18)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
