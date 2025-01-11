7ee3f4ddba49462be2094b66a17008fbc2a36892
Kevin Per <kevin.per@protonmail.com>
2025-01-11 18:32:17 +0000

    lcb: Fixed test

--- vadl/test/vadl/lcb/tablegen/lowering/TableGenInstructionRendererTest.java
+++ vadl/test/vadl/lcb/tablegen/lowering/TableGenInstructionRendererTest.java
@@ -64,7 +64,6 @@ class TableGenInstructionRendererTest extends AbstractTest {
 
     // Then
     assertThat(result).isEqualToIgnoringWhitespace("""
-        
         def nameValue : Instruction
         {
         let Namespace = "namespaceValue";
@@ -97,6 +96,7 @@ class TableGenInstructionRendererTest extends AbstractTest {
         let isCodeGenOnly = 0;
         let mayLoad       = 0;
         let mayStore      = 0;
+        let isBarrier     = 0;
                 
         let Constraints = "";
         let AddedComplexity = 0;
