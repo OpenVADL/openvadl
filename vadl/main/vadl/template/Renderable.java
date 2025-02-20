b438abbdbedb7174e63b59444c0caf9dfc05b191
Johannes Zottele <official@johannes-zottele.at>
2025-02-20 18:54:51 +0100

    native: Add the Renderable interface that allows classes to be renderable

---
+++ vadl/main/vadl/template/Renderable.java
@@ -0,0 +1,9 @@
+package vadl.template;
+
+import java.util.Map;
+
+public interface Renderable {
+
+  Map<String, Object> renderObj();
+
+}
--- vadl/main/vadl/template/VariableChecker.java
+++ vadl/main/vadl/template/VariableChecker.java
@@ -1,56 +1,85 @@
 package vadl.template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
 import org.apache.commons.lang3.ClassUtils;
-import vadl.viam.Specification;
+import vadl.utils.Pair;
 
 class VariableChecker {
 
-  static void checkVariables(Map<String, Object> variables) {
+  static void checkVariables(Map<String, Object> variables) throws IllegalRenderTypeException {
+    var entriesToPut = new ArrayList<Pair<String, Object>>();
     for (Map.Entry<String, Object> entry : variables.entrySet()) {
-      check(entry.getValue());
+      var newVal = check(entry.getValue());
+      if (newVal != null && newVal != entry.getValue()) {
+        entriesToPut.add(new Pair<>(entry.getKey(), newVal));
+      }
+    }
+    if (!entriesToPut.isEmpty()) {
+      // copy as map might be immutable
+      variables = new HashMap<>(variables);
+    }
+    for (var entry : entriesToPut) {
+      variables.put(entry.left(), entry.right());
     }
   }
 
-  private static void check(@Nullable Object object) {
+  private static @Nullable Object check(@Nullable Object object) throws IllegalRenderTypeException {
     if (object == null) {
-      return;
+      return null;
     }
     if (ClassUtils.isPrimitiveOrWrapper(object.getClass())) {
-      return;
+      return object;
     }
     if (object instanceof String) {
-      return;
+      return object;
     }
     if (object instanceof Map<?, ?> map) {
-      check(map);
-      return;
+      return check(map);
     }
     if (object instanceof List<?> list) {
-      check(list);
-      return;
+      return check(list);
     }
-    throw new IllegalArgumentException(
+    if (object instanceof Renderable renderable) {
+      return check(renderable.renderObj());
+    }
+    throw new IllegalRenderTypeException(
         "Unsupported rendering type: " + object + " of type " + object.getClass());
   }
 
-  private static void check(Map<?, ?> map) {
+  private static Object check(Map<?, ?> map) throws IllegalRenderTypeException {
+    var newMap = new HashMap<String, Object>();
     for (var entry : map.entrySet()) {
-      if (!(entry.getKey() instanceof String)) {
-        throw new IllegalArgumentException(
+      if (!(entry.getKey() instanceof String key)) {
+        throw new IllegalRenderTypeException(
             "All map keys that are getting rendered must be a String. Found: " + entry.getKey());
       }
-      check(entry.getValue());
+      newMap.put(key, check(entry.getValue()));
     }
+    return newMap;
   }
 
-  private static void check(List<?> map) {
-    for (var object : map) {
-      check(object);
-    }
+  private static Object check(List<?> map) {
+    return map.stream().map(VariableChecker::check).toList();
+  }
+}
+
+class IllegalRenderTypeException extends RuntimeException {
+  public IllegalRenderTypeException() {
   }
 
+  public IllegalRenderTypeException(String message) {
+    super(message);
+  }
 
+  public IllegalRenderTypeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public IllegalRenderTypeException(Throwable cause) {
+    super(cause);
+  }
 }
