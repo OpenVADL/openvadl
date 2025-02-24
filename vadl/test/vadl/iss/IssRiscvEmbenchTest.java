d5a6fb92c91ee8918d309ffc823cc963b3d449d6
Johannes Zottele <official@johannes-zottele.at>
2025-02-24 17:49:50 +0100

    iss-testing: Control embench MHz via EMBENCH_MHZ env variable

--- vadl/test/vadl/iss/IssRiscvEmbenchTest.java
+++ vadl/test/vadl/iss/IssRiscvEmbenchTest.java
@@ -2,45 +2,48 @@ package vadl.iss;
 
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.utility.MountableFile;
 import vadl.pass.exception.DuplicatedPassKeyException;
 import vadl.utils.VadlFileUtils;
 
 public class IssRiscvEmbenchTest extends QemuIssTest {
 
+  private static final Logger log = LoggerFactory.getLogger(IssRiscvEmbenchTest.class);
+
   @Test
   void rv64imEmbenchTest() throws IOException {
-    var image = generateIssSimulator("sys/risc-v/rv64im.vadl");
-
-    // load embench from resources
-    var embenchPath = VadlFileUtils.copyResourceDirToTempDir("embench", "embench");
-
-    // build benchmarks for rv64i spike and run benchmarks in generated iss
-    var runCommand = "chmod -R +x /work/embench "
-        + "&& cd /work/embench "
-        + "&& bash ./build_spike-rv64im.sh "
-        + "&& bash ./benchmark_qemu.sh qemu-system-rv64im -nographic -M virt -bios";
-
-    runContainer(image,
-        container -> container
-            .withCopyFileToContainer(MountableFile.forHostPath(embenchPath), "/work/embench")
-            .withCommand("/bin/bash", "-c", runCommand),
-        null);
+    runEmbenchTest("sys/risc-v/rv64im.vadl", "build_spike-rv64im.sh", "qemu-system-rv64im");
   }
-
 
   @Test
   void rv32imEmbenchTest() throws IOException {
-    var image = generateIssSimulator("sys/risc-v/rv32im.vadl");
+    runEmbenchTest("sys/risc-v/rv32im.vadl", "build_spike-rv32im.sh", "qemu-system-rv32im");
+  }
 
-    // load embench from resources
+  private void runEmbenchTest(String vadlPath, String buildScript, String qemuSystem)
+      throws IOException {
+    var image = generateIssSimulator(vadlPath);
+
+    var mhz = 50;
+    var envMhz = System.getenv("EMBENCH_MHZ");
+    if (envMhz != null) {
+      mhz = Integer.parseInt(envMhz);
+    }
+    
+    log.info("Setting up embench with {} mhz", mhz);
+
+    // Load embench from resources
     var embenchPath = VadlFileUtils.copyResourceDirToTempDir("embench", "embench");
 
-    // build benchmarks for rv64i spike and run benchmarks in generated iss
-    var runCommand = "chmod -R +x /work/embench "
-        + "&& cd /work/embench "
-        + "&& bash ./build_spike-rv32im.sh "
-        + "&& bash ./benchmark_qemu.sh qemu-system-rv32im -nographic -M virt -bios";
+    // Build benchmarks and run them in the generated ISS
+    var runCommand = String.format(
+        "chmod -R +x /work/embench && cd /work/embench "
+            + "&& bash ./%s --cpu-mhz=" + mhz
+            + "&& bash ./benchmark_qemu.sh %s -nographic -M virt -bios",
+        buildScript, qemuSystem
+    );
 
     runContainer(image,
         container -> container
