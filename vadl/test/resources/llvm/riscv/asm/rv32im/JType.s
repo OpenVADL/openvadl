ebecab2a401cff9578f822380ae2a209301c9d22
Benjamin Kasper <e12122530@student.tuwien.ac.at>
2025-03-05 17:44:17 +0100

    asmparser: Add immediate value edge case tests

--- vadl/test/resources/llvm/riscv/asm/rv32im/JType.s
+++ vadl/test/resources/llvm/riscv/asm/rv32im/JType.s
@@ -4,3 +4,23 @@ JAL x16, 10
 # CHECK: <MCInst #{{[0-9]+}} JAL
 # CHECK-NEXT: <MCOperand Reg:18>
 # CHECK-NEXT: <MCOperand Imm:20>>
+
+# maximum valid immediate value
+JAL x16, 524287
+# CHECK: <MCInst #{{[0-9]+}} JAL
+# CHECK-NEXT: <MCOperand Reg:18>
+# CHECK-NEXT: <MCOperand Imm:1048574>>
+
+# immediate value overflow
+JAL x16, 524288
+# CHECK: error: Invalid immediate operand for JAL.imm. Value {{[0-9]+}} is out of the valid range {{.*}}
+
+# minimum valid immediate value
+JAL x16, -524288
+# CHECK: <MCInst #{{[0-9]+}} JAL
+# CHECK-NEXT: <MCOperand Reg:18>
+# CHECK-NEXT: <MCOperand Imm:-1048576>>
+
+# immediate value underflow
+JAL x16, -524289
+# CHECK: error: Invalid immediate operand for JAL.imm. Value {{[0-9]+}} is out of the valid range {{.*}}
