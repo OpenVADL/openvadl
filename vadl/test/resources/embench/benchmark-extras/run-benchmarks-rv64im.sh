f4dcadd496181c0f03e1733693133544f30f32d5
Benedikt Huber <benedikt_huber@gmx.at>
2025-03-27 16:56:11 +0100

    embench: Use short runs for try out

--- vadl/test/resources/embench/benchmark-extras/run-benchmarks-rv64im.sh
+++ vadl/test/resources/embench/benchmark-extras/run-benchmarks-rv64im.sh
@@ -4,11 +4,13 @@ set -e
 cd $(realpath $(dirname "$0"))
 
 # QEMU
-../build_spike-rv64im.sh --cpu-mhz 100
+../build_spike-rv64im.sh --cpu-mhz 1
 echo "Benchmarking open-vadl..."
+which qemu-system-rv64im
 ./run-benchmark.sh "rv64im-open-vadl"  ./benchmark_qemu.sh       "qemu-system-rv64im" -nographic -M virt -bios
 
 echo "Benchmarking qemu..."
+which qemu-system-riscv64
 ./run-benchmark.sh "rv64im-qemu"       ./benchmark_qemu.sh       "qemu-system-riscv64" -nographic -M spike -bios
 echo "Done."
 
