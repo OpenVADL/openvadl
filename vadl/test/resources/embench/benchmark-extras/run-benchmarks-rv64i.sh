ac0cde81c4871e9028eb49e810c14eedba08ff1b
Johannes Zottele <official@johannes-zottele.at>
2025-04-11 11:29:58 +0200

    iss-testing: Remove -M virt from qemu command

--- vadl/test/resources/embench/benchmark-extras/run-benchmarks-rv64i.sh
+++ vadl/test/resources/embench/benchmark-extras/run-benchmarks-rv64i.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText : © 2025 TU Wien <vadl@tuwien.ac.at>
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 set -e
 
 cd $(realpath $(dirname "$0"))
@@ -8,7 +24,7 @@ cd $(realpath $(dirname "$0"))
 echo "Benchmarking qemu..."
 ./run-benchmark.sh "rv64i-qemu"       ./benchmark_qemu.sh       "qemu-system-riscv64" -nographic -M spike -bios
 echo "Benchmarking open-vadl..."
-./run-benchmark.sh "rv64i-open-vadl"  ./benchmark_qemu.sh       "qemu-system-rv64i" -nographic -M virt -bios
+./run-benchmark.sh "rv64i-open-vadl"  ./benchmark_qemu.sh       "qemu-system-rv64i" -nographic -bios
 echo "Done."
 
 # Normalize dtc timings
