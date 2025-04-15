46c3f5f4e6a19ca48a14735fb4d18eae3fde9701
Giftzwerg02 <chessplayer1@gmx.at>
2025-04-15 20:37:22 +0200

    iss: Switched to IPC using SHM and SEM, implemented a basic lockstepping programm at TB-level

--- vadl/main/resources/templates/iss/contrib/plugins/cosimulation.c
+++ vadl/main/resources/templates/iss/contrib/plugins/cosimulation.c
@@ -1,166 +1,142 @@
+#include "sys/shm.h"
+#include <fcntl.h>
 #include <gio/gio.h>
 #include <glib.h>
 #include <json-glib/json-glib.h>
 #include <qemu-plugin.h>
+#include <semaphore.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
+
+#define SHMSTRING_MAX_LEN 256
+#define TBINFO_ENTRIES 1024
+#define TBINSNINFO_ENTRIES 32
 
 static qemu_plugin_id_t plugin_id;
 static unsigned long bb_count;
 static unsigned long insn_count;
 
 typedef struct {
-  uint64_t pc;
-  size_t insns;
-  GSList *insns_info;
-} TBInfo;
+  size_t len;
+  char value[SHMSTRING_MAX_LEN];
+} SHMString;
 
 typedef struct {
   uint64_t pc;
   size_t size;
-  const char *symbol;
-  void *hwaddr;
-  char *disas;
+  SHMString symbol;
+  SHMString hwaddr;
+  SHMString disas;
 } TBInsnInfo;
 
 typedef struct {
-  gchar *hostname;
-  guint16 port;
+  uint64_t pc;
+  size_t insns;
+  size_t insns_info_size;
+  TBInsnInfo insns_info[TBINSNINFO_ENTRIES];
+} TBInfo;
+
+typedef struct {
+  const gchar *client_id;
 } Arguments;
 
-static GSocketConnection *connection;
+typedef struct {
+  size_t size;
+  TBInfo infos[TBINFO_ENTRIES];
+} BrokerSHM;
+
 static Arguments args;
+static BrokerSHM *shm;
+static sem_t *sem_client, *sem_server;
 
-static JsonNode *serialize_insn_info(TBInsnInfo *insn_info) {
-  JsonBuilder *builder = json_builder_new();
-
-  json_builder_begin_object(builder);
-  json_builder_set_member_name(builder, "pc");
-  json_builder_add_int_value(builder, insn_info->pc);
-
-  json_builder_set_member_name(builder, "size");
-  json_builder_add_int_value(builder, insn_info->size);
-
-  if (insn_info->symbol) {
-    json_builder_set_member_name(builder, "symbol");
-    json_builder_add_string_value(builder, insn_info->symbol);
+static void open_sems(void) {
+  gchar *sem_client_name =
+      g_strdup_printf("cosimulation-sem-client-%s", args.client_id);
+  sem_client = sem_open(sem_client_name, O_RDWR);
+  if (sem_client == SEM_FAILED) {
+    g_error("failed to open sem_client for client: %s", args.client_id);
+    return;
   }
 
-  if (insn_info->disas) {
-    json_builder_set_member_name(builder, "disas");
-    json_builder_add_string_value(builder, insn_info->disas);
+  gchar *sem_server_name =
+      g_strdup_printf("cosimulation-sem-server-%s", args.client_id);
+  sem_server = sem_open(sem_server_name, O_RDWR);
+  if (sem_server == SEM_FAILED) {
+    g_error("failed to open sem_server for client: %s", args.client_id);
+    return;
   }
-
-  char addr_str[17]; 
-  sprintf(addr_str, "%016llx", (unsigned long long)insn_info->hwaddr);
-  json_builder_set_member_name(builder, "hwaddr");
-  json_builder_add_string_value(builder, addr_str);
-
-  json_builder_end_object(builder);
-
-  JsonNode *node = json_builder_get_root(builder);
-
-  g_object_unref(builder);
-
-  return node;
-}
-
-static char *tbinfo_to_json(TBInfo *tb_info) {
-  JsonBuilder *builder = json_builder_new();
-
-  json_builder_begin_object(builder);
-  json_builder_set_member_name(builder, "pc");
-  json_builder_add_int_value(builder, tb_info->pc);
-
-  json_builder_set_member_name(builder, "insns");
-  json_builder_add_int_value(builder, tb_info->insns);
-
-  json_builder_set_member_name(builder, "insns_info");
-  json_builder_begin_array(builder);
-
-  GSList *iter;
-  for (iter = tb_info->insns_info; iter != NULL; iter = iter->next) {
-    TBInsnInfo *insn_info = iter->data;
-    JsonNode *node = serialize_insn_info(insn_info);
-    json_builder_add_value(builder, node);
-  }
-
-  json_builder_end_array(builder);
-  json_builder_end_object(builder);
-
-  JsonGenerator *gen = json_generator_new();
-  JsonNode *root = json_builder_get_root(builder);
-  json_generator_set_root(gen, root);
-  gchar *json_str = json_generator_to_data(gen, NULL);
-
-  json_node_free(root);
-  g_object_unref(gen);
-  g_object_unref(builder);
-
-  return json_str;
-}
-
-static void plugin_cleanup(qemu_plugin_id_t id) {
-  qemu_plugin_outs("::plugin_cleanup");
 }
 
 static void plugin_exit(qemu_plugin_id_t id, void *p) {
-  qemu_plugin_outs("::plugin_exit");
-  plugin_cleanup(id);
+  printf("plugin_exit\n");
 }
 
-static guint16 parse_port(const char *string) {
-  gchar *endptr = NULL;
-  guint64 port = g_ascii_strtoull(string, &endptr, 10);
-  if (endptr != NULL && endptr == string) {
-    fprintf(stderr, "parse_port::failed to parse port: %s\n", string);
-    return EXIT_FAILURE;
-  }
-
-  if (port <= 0 || port > G_MAXUINT16) {
-    fprintf(stderr, "parse_port::invalid port number: %s\n", string);
-    return EXIT_FAILURE;
-  }
-
-  return port;
-}
-
-static GSocketConnection *connect_to_broker(void) {
-  GError *error = NULL;
-  GSocketClient *client = g_socket_client_new();
-
-  GSocketConnection *connection = g_socket_client_connect_to_host(
-      client, args.hostname, args.port,
-      NULL, // Cancellable gobject, might be useful later
-      &error);
-
-  if (error != NULL) {
-    g_error("%s", error->message);
-    // FIXME: retry-policy and/or crash
+// Connects to the broker by accessing the assigned shared memory
+// The shared memory is located under /cosimulation/shm-{client_id}
+static BrokerSHM *connect_to_broker(void) {
+  gchar *shm_name = g_strdup_printf("/cosimulation-shm-%s", args.client_id);
+  int shm_fd = shm_open(shm_name, O_RDWR, 0600);
+  if (shm_fd == -1) {
+    g_error("failed to open shared memory for client: %s", args.client_id);
     return NULL;
   }
 
-  g_print("Client connected to broker\n");
-
-  if (error != NULL) {
-    g_error("connect_to_broker::g_output_stream_write: %s", error->message);
+  if (ftruncate(shm_fd, sizeof(BrokerSHM)) == -1) {
+    g_error("failed to truncate shared memory for client: %s", args.client_id);
     return NULL;
   }
 
-  return connection;
+  BrokerSHM *shm = mmap(NULL, sizeof(BrokerSHM), PROT_READ | PROT_WRITE,
+                        MAP_SHARED, shm_fd, 0);
+  if (shm == MAP_FAILED) {
+    g_error("failed to mmap jshared memory for client: %s", args.client_id);
+    return NULL;
+  }
+
+  return shm;
+}
+
+static void vcpu_tb_exec(unsigned int cpu_index, void *udata) {
+  // printf("vcpu exec\n");
+  // GArray *regs = qemu_plugin_get_registers();
+  // printf("in vcpu_tb_exec with: %d registers\n", regs->len);
+  // for (size_t i = 0; i < regs->len; i++) {
+  //   printf("starting to print reg: %lu\n", i);
+  //   const qemu_plugin_reg_descriptor *desc = 
+  //       g_array_index(regs, qemu_plugin_reg_descriptor *, i);
+  //
+  //   if()
+  //   
+  //   printf("starting to print reg\n");
+  //   printf("ptr: %p\n", desc);
+  //   printf("Register %s: ", desc->name);
+  //
+  //   // Read register value
+  //   GByteArray *buf = g_byte_array_new();
+  //   qemu_plugin_read_register(desc->handle, buf);
+  //   printf("%lu\n", *(uint64_t *)buf->data);
+  //   printf("done to print reg: %lu\n", i);
+  // }
+  //
+  // g_array_free(regs, TRUE);
 }
 
 static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
-  GError *error = NULL;
-  GOutputStream *ostream =
-      g_io_stream_get_output_stream(G_IO_STREAM(connection));
+  sem_wait(sem_client);
 
   uint64_t pc = qemu_plugin_tb_vaddr(tb);
   size_t insns = qemu_plugin_tb_n_insns(tb);
 
-  GSList *insn_infos;
+  TBInfo tbinfo;
+  tbinfo.pc = pc;
+  tbinfo.insns = insns;
+
   for (int i = 0; i < insns; i++) {
     // qemu_plugin_insn_data();
     struct qemu_plugin_insn *insn = qemu_plugin_tb_get_insn(tb, i);
@@ -173,38 +149,69 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
     TBInsnInfo insn_info;
     insn_info.pc = insn_pc;
     insn_info.size = insn_size;
-    insn_info.symbol = insn_symbol;
-    insn_info.hwaddr = insn_hwaddr;
-    insn_info.disas = insn_disas;
 
-    insn_infos = g_slist_append(insn_infos, &insn_info);
+    SHMString symbol = {};
+    SHMString hwaddr = {};
+    SHMString disas = {};
+
+    if (insn_symbol != NULL) {
+      strncpy(symbol.value, insn_symbol, SHMSTRING_MAX_LEN);
+      symbol.len = strlen(symbol.value);
+    }
+
+    if (insn_hwaddr != NULL) {
+      char *hwaddrfmt = g_strdup_printf("%p", insn_hwaddr);
+      strncpy(hwaddr.value, hwaddrfmt, SHMSTRING_MAX_LEN);
+      hwaddr.len = strlen(hwaddr.value);
+    }
+
+    if (insn_disas != NULL) {
+      strncpy(disas.value, insn_disas, SHMSTRING_MAX_LEN);
+      disas.len = strlen(disas.value);
+    }
+
+    insn_info.symbol = symbol;
+    insn_info.hwaddr = hwaddr;
+    insn_info.disas = disas;
+
+    tbinfo.insns_info[i] = insn_info;
   }
 
-  TBInfo tbinfo;
-  tbinfo.pc = pc;
-  tbinfo.insns = insns;
-  tbinfo.insns_info = insn_infos;
+  tbinfo.insns_info_size = insns;
 
-  char *json_msg = tbinfo_to_json(&tbinfo);
-  if (json_msg == NULL) {
-    g_error("failed to convert tbinfo to json");
-    return;
-  }
+  shm->infos[shm->size] = tbinfo;
+  shm->size = shm->size + 1;
 
-  g_output_stream_write(ostream, json_msg, strlen(json_msg), NULL, &error);
+  qemu_plugin_register_vcpu_tb_exec_cb(tb, vcpu_tb_exec, QEMU_PLUGIN_CB_R_REGS,
+                                       NULL);
 
-  if (error != NULL) {
-    g_error("connect_to_broker::g_output_stream_write: %s", error->message);
-  }
+  sem_post(sem_server);
+}
+
+static void vcpu_init(qemu_plugin_id_t id, unsigned int vcpu_index) {
+    // CPU *c;
+    //
+    // g_rw_lock_writer_lock(&expand_array_lock);
+    // if (vcpu_index >= cpus->len) {
+    //     g_array_set_size(cpus, vcpu_index + 1);
+    // }
+    // g_rw_lock_writer_unlock(&expand_array_lock);
+    //
+    // c = get_cpu(vcpu_index);
+    // c->last_exec = g_string_new(NULL);
+    // c->registers = registers_init(vcpu_index);
+
+}
+
+static void vcpu_exit(qemu_plugin_id_t id, unsigned int vcpu_index) {
+  printf("vcpu exiting: %d...\n", vcpu_index);
+  fflush(stdout);
 }
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
                                            const qemu_info_t *info, int argc,
                                            char **argv) {
-  qemu_plugin_outs("::qemu_plugin_install");
-
-  // set default option-values
-  args.hostname = (gchar *)"localhost";
+  printf("::qemu_plugin_install\n");
 
   // parse options
   for (int i = 0; i < argc; i++) {
@@ -212,8 +219,8 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
     g_auto(GStrv) tokens = g_strsplit(p, "=", 2);
     const char *argname = tokens[0];
     const char *argvalue = tokens[1];
-    if (g_strcmp0(argname, "port") == 0) {
-      args.port = parse_port(argvalue);
+    if (g_strcmp0(argname, "client-id") == 0) {
+      args.client_id = strdup(argvalue);
     } else {
       fprintf(stderr, "option parsing failed: %s\n", p);
       return EXIT_FAILURE;
@@ -221,19 +228,27 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
   }
 
   // check required options
-  if (args.port == 0) {
-    fprintf(stderr, "option port=<uint16_t> is required, no port was given\n");
+  if (args.client_id == NULL) {
+    fprintf(stderr,
+            "option client-id=<gchar*> is required, no client-id was given\n");
     return EXIT_FAILURE;
   }
 
-  connection = connect_to_broker();
-  if (connection == NULL) {
+  shm = connect_to_broker();
+  if (shm == NULL) {
+    return EXIT_FAILURE;
+  }
+
+  open_sems();
+  if (sem_client == NULL || sem_server == NULL) {
     return EXIT_FAILURE;
   }
 
   plugin_id = id;
 
   qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
-  // qemu_plugin_register_atexit_cb(id, qemu_plugin_udata_cb_t cb, void *userdata);
+  qemu_plugin_register_vcpu_init_cb(id, vcpu_init);
+  qemu_plugin_register_vcpu_exit_cb(id, vcpu_exit);
+  qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
   return EXIT_SUCCESS;
 }
--- vadl/test/resources/scripts/iss_qemu/cosimulation_broker.py
+++ vadl/test/resources/scripts/iss_qemu/cosimulation_broker.py
@@ -1,67 +1,218 @@
-import socket
+from ctypes import POINTER, c_char, c_char_p, c_uint64, c_void_p, sizeof, Structure, c_int, c_size_t
 from dataclasses import dataclass
 import argparse
 import sys
+import mmap
+import subprocess
+import posix_ipc as ipc
+import atexit
 
 import logging
 from typing import Optional
-import json
+
+reference_ricsv64_exe = "/qemu-system-riscv64"
 
 @dataclass
 class Options:
     qemu_dir: str
-    port: int
     clients: int
+    test_exec: str
+
+class SHMString(Structure):
+    MAX_LEN = 256
+    _fields_ = [("len", c_size_t), ("value", c_char * MAX_LEN)]
+
+    def __repr__(self):
+        values = ", ".join(f"{name}={value}" 
+                          for name, value in self._asdict().items())
+        return f"<{self.__class__.__name__}: {values}>"
+    
+    def _asdict(self):
+        return {field[0]: getattr(self, field[0]) 
+                for field in self._fields_}
+
+class TBInsnInfo(Structure):
+    _fields_ = [("pc", c_uint64), ("size", c_size_t), ("symbol", SHMString), ("hwaddr", SHMString), ("disas", SHMString)]
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: pc={self.pc}, size={self.size}, symbol={self.symbol}, hwaddr={self.hwaddr}, disas={self.disas}>"
+
+    
+    def _asdict(self):
+        return {field[0]: getattr(self, field[0]) 
+                for field in self._fields_}
+
+class TBInfo(Structure):
+    INSNS_INFOS_SIZE = 32
+    _fields_ = [("pc", c_uint64), ("insns", c_size_t), ("insns_info_size", c_size_t), ("insns_info", TBInsnInfo * INSNS_INFOS_SIZE)]
+
+    def __repr__(self):
+        values = f"pc={self.pc}, insns={self.insns}, insns_info_size={self.insns_info_size}, insns_info={self.insns_info[:self.insns_info_size]}"
+        return f"<{self.__class__.__name__}: {values}>"
+    
+    def _asdict(self):
+        return {field[0]: getattr(self, field[0]) 
+                for field in self._fields_}
+
+
+class SHMStruct(Structure):
+    INFOS_SIZE = 1024
+    _fields_ = [("size", c_size_t), ("infos", TBInfo * INFOS_SIZE)]
+
+    def __repr__(self):
+        values = f"size={self.size}, infos={self.infos[:self.size]}"
+        return f"<{self.__class__.__name__}: {values}>"
+    
+    def _asdict(self):
+        return {field[0]: getattr(self, field[0]) 
+                for field in self._fields_}
+
+def format_tb_insn_info(insn: TBInsnInfo) -> str:
+    _fields_ = [("pc", c_uint64), ("size", c_size_t), ("symbol", c_char_p), ("hwaddr", c_void_p), ("disas", c_char_p)]
+    return f"TBInsn"
+
+def format_tb_info(tb: TBInfo):
+    pass
+
+def format_shm(shm: SHMStruct):
+    pass
 
 @dataclass
 class Client:
     id: int
-    socket: socket.socket
+    shm: ipc.SharedMemory
+    shm_struct: SHMStruct
+    sem_server: ipc.Semaphore
+    sem_client: ipc.Semaphore
+    is_open: bool
+    # process: subprocess.Popen[bytes]
 
-@dataclass
-class TBInsnInfo:
-    pc: int
-    size: int
-    symbol: str
-    hwaddr: str
-    disas: str
+clients: list[Client] = []
 
-@dataclass
-class TBInfo:
-    pc: int
-    insns: int
-    insns_info: list[TBInsnInfo]
+import threading
+import subprocess
 
+def run_with_callback(command, on_complete, options: Options, client: Client):
+    def runner():
+        process = subprocess.Popen(command)
+        process.wait()
+        on_complete(process.returncode, options, client)
+    
+    thread = threading.Thread(target=runner)
+    thread.start()
+    return thread
 
+# Usage
+def callback(returncode: int, options: Options, client: Client):
+    print(f"Process (client: {client.id}) finished with code: {returncode}")
+    # keep smh alive for reporting 
+    cleanup_sem(options, client)
+
+# reading is possible if a client is done
 def read(client: Client) -> Optional[TBInfo]:
-    data = client.socket.recv(1024)
-    if not data:
-        return None
+    client.sem_server.acquire()
+    smh = client.shm_struct
+    for info in smh.infos:
+        for insn in info.insns_info:
+            if insn.pc > 0:
+                logger.info(f"Reading client #{client.id}: {insn.disas.value.decode()}")
 
-    json_obj = json.loads(data)
-    tbinfo = TBInfo(**json_obj)
+    client.sem_client.release()
 
-    logger.debug(f"Received from client (id={client.id}): {tbinfo}")
+    return None
 
-    return tbinfo
-
+def collect_disas(client: Client) -> str:
+    disas = []
+    smh = client.shm_struct
+    for info in smh.infos:
+        for insn in info.insns_info:
+            if insn.pc > 0:
+                disas.append(insn.disas.value.decode())
+    return "\n".join(disas)
 
 def main(options: Options):
     logger.debug(f"starting broker: options={options}")
-    clients: list[Client] = []
 
-    logger.info(f"starting server on port={options.port}")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(("127.0.0.1", options.port))
-        s.listen()
-        for i in range(options.clients):
-            conn, _ = s.accept()
-            client = Client(i, socket=conn)
-            clients += [client]
-            logger.info(f"{i+1}/{options.clients} clients connected")
+    # create shared memory and semaphores per client
+    for i in range(options.clients):
+        shm = ipc.SharedMemory(f"/cosimulation-shm-{i}", ipc.O_CREX, size=sizeof(SHMStruct))
+        mm = mmap.mmap(shm.fd, sizeof(SHMStruct))
+        shm_struct = SHMStruct.from_buffer(mm) 
 
-        logger.info("All clients connected, starting cosimulation...")
+        sem_server = ipc.Semaphore(f"/cosimulation-sem-server-{i}", ipc.O_CREX)
+        sem_client = ipc.Semaphore(f"/cosimulation-sem-client-{i}", ipc.O_CREX)
+        logger.info(f"created shm and sems, spawning client with id: {i}")
+
+        executable_path = f"{options.qemu_dir}/{reference_ricsv64_exe}"
+        plugin_path = f"{options.qemu_dir}/contrib/plugins/libcosimulation.so,client-id={i}"
+        args = ["-M", "spike", "-nographic", "-bios", options.test_exec, "-plugin", plugin_path]
+        logger.info(f"starting client: {" ".join([executable_path, *args])}")
+        client = Client(i, shm, shm_struct, sem_server=sem_server, sem_client=sem_client, is_open=True)
+        clients.append(client)
+        run_with_callback([executable_path, *args], callback, options, client)
+
+
+
+    while any(map(lambda c: c.is_open, clients)):
+        # lockstepping
+        for client in clients: 
+            if client.is_open:
+                try:
+                    client.sem_client.release() # start client
+                    read(client)                # read from client[0] after done
+                except ipc.ExistentialError: # something was closed, mark client as done
+                    client.is_open = False
+
+    print("----- REPORT START -----")
+
+    clients_disas = []
+    for client in clients:
+        print(f"----- CLIENT #{client.id} START -----")
+        disas = collect_disas(client)
+        clients_disas.append(disas)
+        print(disas)
+        print(f"----- CLIENT #{client.id} END -----")
+
+    first_disas = clients_disas[0]
+    all_equal = True
+    for disas in clients_disas[1:]:
+        if first_disas != disas:
+            all_equal = False
+
+    print("----- REPORT SUMMARY -----")
+    if all_equal:
+        print("Result: All clients returned the same output.")
+    else:
+        print("Result: All clients returned the same output.")
+    print("----- REPORT SUMMARY -----")
+
+    print("----- REPORT END -----")
+
+def cleanup_sem(options: Options, client: Client):
+    try:
+        client.sem_client.unlink()
+        client.sem_server.unlink()
+        client.sem_client.close()
+        client.sem_server.close()
+    except ipc.ExistentialError:
+        pass # ignore
+
+def cleanup_smh(options: Options, client: Client):
+    try:
+        client.shm.close_fd()
+        client.shm.unlink()
+    except ipc.ExistentialError:
+        pass # ignore
+
+def cleanup_client(options: Options, client: Client):
+    cleanup_sem(options, client)
+    cleanup_smh(options, client)
+
+def cleanup(options: Options):
+    logger.info("cleaning up shm and sems for each client")
+    for client in clients:
+        cleanup_client(options, client)
+
 
 
 if __name__ == '__main__':
@@ -74,15 +225,17 @@ if __name__ == '__main__':
         prog="Cosimulation Broker",
         description="Executes two (or more) qemu-instances in parallel which need to use the cosimulation plugin to connect to the broker."
     )
-    parser.add_argument("-p", "--port", type=int, help="Port on which the broker should receive connections on", required=True)
     parser.add_argument('--qemu-dir', type=str, help="Directory of the compiled qemu project", default="./build")
     parser.add_argument('-c', '--clients', type=int, help="The number of qemu clients to handle", default=2)
+    parser.add_argument('--test-exec', type=str, help="The compiled executable that should be used as the input for each qemu instance.", required=True)
     args = parser.parse_args()
     options = Options(
-        port=args.port,
         qemu_dir=args.qemu_dir,
-        clients=args.clients
+        clients=args.clients,
+        test_exec=args.test_exec
     )
+
+    atexit.register(cleanup, options)
 
     main(options)
 
