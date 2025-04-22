c2114ace4c18f8b5de10cc67444fa50e3fc481b2
Kevin Per <kevin.per@protonmail.com>
2025-04-22 12:00:35 +0000

    lcb: Use ISS images for LCB

--- vadl/test/resources/images/spike_rv64im/base_image/lcb_wrapper.sh
+++ vadl/test/resources/images/spike_rv64im/base_image/lcb_wrapper.sh
@@ -10,7 +10,7 @@ set -x
 # Not sure, why though? Maybe, the linker script but all .text are next to each other.
 # Maybe, the tohost section is too far away.
 /src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -fPIC -c -O0 /helper/common.c -o /helper/common.o
-/opt/riscv-cross/bin/riscv64-unknown-linux-gnu-gcc -static -nostartfiles -T/helper/link_lcbw.ld /tmp/main.o /helper/init.o /helper/trap.o /helper/vars.spike.o /helper/common.o -o /tmp/main
+/opt/riscv-cross/bin/riscv64-unknown-elf-gcc -static -nostartfiles -T/helper/link_lcbw.ld /tmp/main.o /helper/init.o /helper/trap.o /helper/vars.spike.o /helper/common.o -o /tmp/main
 
 echo "Running spike..."
 timeout --preserve-status 5 fail_if "FAILED" /opt/spike/bin/spike --isa=${SPIKE_TARGET} /tmp/main
