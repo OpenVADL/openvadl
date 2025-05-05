71dcf083682cc1f39f2263d96573755a47b27d77
Kevin Per <kevin.per@protonmail.com>
2025-05-05 15:02:44 +0000

    lcb: Updated filename

--- vadl/test/resources/images/lcb_execution_test_rv64im/base_image/lcb_wrapper.sh
+++ vadl/test/resources/images/lcb_execution_test_rv64im/base_image/lcb_wrapper.sh
@@ -3,7 +3,7 @@
 set -x
 
 /src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -march=${TARGET} -fPIC -c -O0 /tmp/main.s -o /tmp/main.o
-/src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -march=${TARGET} -c -O0 /helper/init.s -o /helper/init.o
+/src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -march=${TARGET} -c -O0 /helper/init.S -o /helper/init.o
 /opt/riscv-cross/bin/riscv64-unknown-elf-gcc -march=${TARGET} -mabi=${ABI} -static -nostartfiles -T/helper/link.ld /tmp/main.o /helper/init.o -o /tmp/main
 
 echo "Running spike..."
