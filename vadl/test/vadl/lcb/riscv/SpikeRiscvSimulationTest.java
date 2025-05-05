46aaea7709a33f05303c8971c6c3cd5eebdbe77b
Kevin Per <kevin.per@protonmail.com>
2025-05-05 16:35:44 +0000

    lcb: Make QEMU riscv64 work
    ci: Updated base image
    
    lcb: Updated image
    
    lcb: Updated image
    
    lcb: Make qemu riscv64 work

--- vadl/test/vadl/lcb/riscv/SpikeRiscvSimulationTest.java
+++ vadl/test/vadl/lcb/riscv/SpikeRiscvSimulationTest.java
@@ -69,7 +69,7 @@ public abstract class SpikeRiscvSimulationTest extends AbstractLcbTest {
 
   private @Nonnull List<DynamicTest> run(int optLevel)
       throws IOException, DuplicatedPassKeyException {
-    var doDebug = false;
+    var doDebug = true;
     var target = getTarget();
     var upstreamBuildTarget = getUpstreamBuildTarget();
     var upstreamClangTarget = getUpstreamClangTarget();
