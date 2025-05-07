193d73ce2d7891e2e99d0276dff3e153153806a3
Giftzwerg02 <chessplayer1@gmx.at>
2025-05-07 10:13:44 +0200

    iss: Implemented basic tb_exec callback to compare cpu states

---
+++ vadl/test/resources/scripts/iss_qemu/cosimulation/config.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass, field
+import tomllib
+from typing import Literal 
+import dacite
+
+@dataclass
+class Logging:
+    level: str
+    file: str = "cosim.log"
+    enable: bool = True
+
+@dataclass
+class Out:
+    dir: str = "./out"
+    format: str = "json"
+
+@dataclass
+class Protocol:
+    mode: Literal["lockstep"]
+    layer: Literal["tb", "exec"]
+    take_all_instructions: bool
+    take_n_instructions: int
+    skip_n_instructions: int = 0
+    out: Out = field(default_factory=Out)
+
+@dataclass
+class Testing:
+    test_exec: str
+    protocol: Protocol
+
+@dataclass
+class Client:
+    exec: str
+    additional_args: list[str]
+
+@dataclass
+class Qemu:
+    plugin: str
+    endian: Literal["big", "little"] 
+    clients: list[Client]
+
+@dataclass
+class Config:
+    qemu: Qemu
+    testing: Testing
+    logging: Logging
+
+def load_config(path: str) -> Config | None:
+    with open(path, mode="rb") as config_file:
+        data = tomllib.load(config_file)
+        try:
+            return dacite.from_dict(data_class=Config, data=data)
+        except TypeError as e:
+            print(f"Missing required field: {e}")
+        except Exception as e:
+            print(f"Error while loading config: {e}")
+
