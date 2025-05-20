a65089115f1d9599309a1e8c6b27f15a51a74e9a
Johannes Zottele <official@johannes-zottele.at>
2025-05-20 16:38:09 +0200

    viam-testing: Remove velocity test preprocessor from abstract test

--- vadl/test/vadl/viam/passes/TypeCastEliminationTest.java
+++ vadl/test/vadl/viam/passes/TypeCastEliminationTest.java
@@ -20,16 +20,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static vadl.utils.GraphUtils.getSingleNode;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.testcontainers.shaded.com.google.errorprone.annotations.concurrent.LazyInit;
 import vadl.AbstractTest;
-import vadl.TestFrontend;
 import vadl.TestUtils;
 import vadl.types.BuiltInTable;
 import vadl.utils.ViamUtils;
@@ -49,95 +47,85 @@ import vadl.viam.passes.verification.ViamVerifier;
 
 public class TypeCastEliminationTest extends AbstractTest {
 
-  private static TestFrontend validFrontend;
+  @LazyInit
+  private static Specification spec;
 
-  @BeforeAll
-  static void runValidSpecAndEliminationPass() throws IOException {
-    validFrontend =
-        AbstractTest.runViamSpecificationWithNewFrontend(
-            "passes/typeCastElimination/valid_type_cast_elimination.vadl");
-    ViamVerifier.verifyAllIn(validFrontend.getViam());
+  @BeforeEach
+  void getSpec() throws IOException {
+    spec =
+        runAndGetViamSpecification("passes/typeCastElimination/valid_type_cast_elimination.vadl");
+    ViamVerifier.verifyAllIn(spec);
   }
 
 
-  @ParameterizedTest
-  @MethodSource("testTrivial_Source")
-  void testTrivial_shouldDeleteTypeCastAndMakeArgumentTypeSameAsResultType(String functionName) {
-    var behavior = getTestBehavior(functionName);
-    assertEquals(3, behavior.getNodes().count());
-    var returnNode = getSingleNode(behavior, ReturnNode.class);
-    var paramNode = getSingleNode(behavior, FuncParamNode.class);
-    assertEquals(returnNode.returnType(), paramNode.type());
+  @TestFactory
+  Stream<DynamicTest> testTrivial_shouldDeleteTypeCastAndMakeArgumentTypeSameAsResultType() {
+    return findFuncNameArgumentsByPrefix("Trivial_", spec).map(func ->
+        DynamicTest.dynamicTest(func, () -> {
+          var behavior = getTestBehavior(func);
+          assertEquals(3, behavior.getNodes().count());
+          var returnNode = getSingleNode(behavior, ReturnNode.class);
+          var paramNode = getSingleNode(behavior, FuncParamNode.class);
+          assertEquals(returnNode.returnType(), paramNode.type());
+        }));
   }
 
-  static Stream<Arguments> testTrivial_Source() {
-    return findFuncNameArgumentsByPrefix("Trivial_", validFrontend.getViam());
+  @TestFactory
+  Stream<DynamicTest> testTruncate_shouldUseTruncateNodeInsteadOfTypeCast() {
+    return findFuncNameArgumentsByPrefix("Truncate_", spec).map(func ->
+        DynamicTest.dynamicTest(func, () -> {
+          var behavior = getTestBehavior(func);
+          assertEquals(4, behavior.getNodes().count());
+          assertEquals(1, behavior.getNodes(TruncateNode.class).count());
+        }));
   }
 
-  @ParameterizedTest
-  @MethodSource("testTruncate_Source")
-  void testTruncate_shouldUseTruncateNodeInsteadOfTypeCast(String functionName) {
-    var behavior = getTestBehavior(functionName);
-    assertEquals(4, behavior.getNodes().count());
-    assertEquals(1, behavior.getNodes(TruncateNode.class).count());
-  }
-
-  static Stream<Arguments> testTruncate_Source() {
-    return findFuncNameArgumentsByPrefix("Truncate_", validFrontend.getViam());
-  }
-
-  @ParameterizedTest
-  @MethodSource("testZeroExtend_Source")
-  void testZeroExtend_shouldUseZeroExtendNodeInsteadOfTypeCast(String functionName) {
-    var behavior = getTestBehavior(functionName);
-    assertEquals(4, behavior.getNodes().count());
-    var paramNode = getSingleNode(behavior, FuncParamNode.class);
-    var zeroExtendNode = getSingleNode(behavior, ZeroExtendNode.class);
-    assertEquals(paramNode, zeroExtendNode.value());
-  }
-
-  static Stream<Arguments> testZeroExtend_Source() {
-    return findFuncNameArgumentsByPrefix("ZeroExtend_", validFrontend.getViam());
+  @TestFactory
+  Stream<DynamicTest> testZeroExtend_shouldUseZeroExtendNodeInsteadOfTypeCast() {
+    return findFuncNameArgumentsByPrefix("ZeroExtend_", spec).map(func ->
+        DynamicTest.dynamicTest(func, () -> {
+          var behavior = getTestBehavior(func);
+          assertEquals(4, behavior.getNodes().count());
+          var paramNode = getSingleNode(behavior, FuncParamNode.class);
+          var zeroExtendNode = getSingleNode(behavior, ZeroExtendNode.class);
+          assertEquals(paramNode, zeroExtendNode.value());
+        }));
   }
 
 
-  @ParameterizedTest
-  @MethodSource("testSignedExtend_Source")
-  void testSignExtend_shouldUseSignExtendNodeInsteadOfTypeCast(String functionName) {
-    var behavior = getTestBehavior(functionName);
-    assertEquals(4, behavior.getNodes().count());
-    var paramNode = getSingleNode(behavior, FuncParamNode.class);
-    var signExtendNode = getSingleNode(behavior, SignExtendNode.class);
-    assertEquals(paramNode, signExtendNode.value());
+  @TestFactory
+  Stream<DynamicTest> testSignExtend_shouldUseSignExtendNodeInsteadOfTypeCast() {
+    return findFuncNameArgumentsByPrefix("SignExtend_", spec).map(func ->
+        DynamicTest.dynamicTest(func, () -> {
+          var behavior = getTestBehavior(func);
+          assertEquals(4, behavior.getNodes().count());
+          var paramNode = getSingleNode(behavior, FuncParamNode.class);
+          var signExtendNode = getSingleNode(behavior, SignExtendNode.class);
+          assertEquals(paramNode, signExtendNode.value());
+        }));
   }
 
-  static Stream<Arguments> testSignedExtend_Source() {
-    return findFuncNameArgumentsByPrefix("SignExtend_", validFrontend.getViam());
+
+  @TestFactory
+  Stream<DynamicTest> testBoolCast_shouldReplaceByNegCallWithSecondArg0Const() {
+    return findFuncNameArgumentsByPrefix("BoolCast_", spec).map(func ->
+        DynamicTest.dynamicTest(func, () -> {
+          var behavior = getTestBehavior(func);
+          assertEquals(5, behavior.getNodes().count());
+          var paramNode = getSingleNode(behavior, FuncParamNode.class);
+          var compareNode = getSingleNode(behavior, BuiltInCall.class);
+          var zeroConstant = getSingleNode(behavior, ConstantNode.class);
+          assertEquals(BuiltInTable.NEQ, compareNode.builtIn());
+          assertEquals(0, zeroConstant.constant().asVal().intValue());
+          assertEquals(paramNode, compareNode.arguments().get(0));
+          assertEquals(zeroConstant, compareNode.arguments().get(1));
+          var returnNode = getSingleNode(behavior, ReturnNode.class);
+          assertEquals(compareNode, returnNode.value());
+        }));
   }
 
-  @ParameterizedTest
-  @MethodSource("testBoolCast_Source")
-  void testBoolCast_shouldReplaceByNegCallWithSecondArg0Const(String functionName) {
-    var behavior = getTestBehavior(functionName);
-    assertEquals(5, behavior.getNodes().count());
-    var paramNode = getSingleNode(behavior, FuncParamNode.class);
-    var compareNode = getSingleNode(behavior, BuiltInCall.class);
-    var zeroConstant = getSingleNode(behavior, ConstantNode.class);
-    assertEquals(BuiltInTable.NEQ, compareNode.builtIn());
-    assertEquals(0, zeroConstant.constant().asVal().intValue());
-    assertEquals(paramNode, compareNode.arguments().get(0));
-    assertEquals(zeroConstant, compareNode.arguments().get(1));
-    var returnNode = getSingleNode(behavior, ReturnNode.class);
-    assertEquals(compareNode, returnNode.value());
-  }
-
-  static Stream<Arguments> testBoolCast_Source() {
-    return findFuncNameArgumentsByPrefix("BoolCast_", validFrontend.getViam());
-  }
-
-  @ParameterizedTest
-  @EmptySource
-  void testNonOptimalExample(List<Object> sources) {
+  @Test
+  void testNonOptimalExample() {
     var behavior = getTestBehavior("NonOptimalExample");
     assertEquals(5, behavior.getNodes().count());
     var truncateNode = getSingleNode(behavior, TruncateNode.class);
@@ -148,7 +136,6 @@ public class TypeCastEliminationTest extends AbstractTest {
   }
 
   private Graph getTestBehavior(String testFunction) {
-    var spec = validFrontend.getViam();
     var simpleTypCast = TestUtils.findDefinitionByNameIn(testFunction, spec, Function.class);
     ViamVerifier.verifyAllIn(simpleTypCast);
     var behavior = simpleTypCast.behavior();
@@ -157,15 +144,14 @@ public class TypeCastEliminationTest extends AbstractTest {
     return behavior;
   }
 
-  private static Stream<Arguments> findFuncNameArgumentsByPrefix(String prefix,
-                                                                 Specification spec) {
+  private static Stream<String> findFuncNameArgumentsByPrefix(String prefix,
+                                                              Specification spec) {
     return
         ViamUtils.findDefinitionsByFilter(spec,
                 (def) -> def.identifier.name().startsWith(prefix)
                     && def instanceof Function)
             .stream()
-            .map(Definition::simpleName)
-            .map(Arguments::of);
+            .map(Definition::simpleName);
   }
 
 }
