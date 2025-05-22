fa8c80247b9e3bdb8085708ddfcf134c55cbde37
kper <kevin.per@protonmail.com>
2025-05-22 14:30:34 +0200

    lcb: Use lcb-test-base as new base image for execution tests (#276)

--- vadl/test/resources/embench/__run_spike_rv64gc.sh
+++ vadl/test/resources/embench/__run_spike_rv64gc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-OUTPUT=$( { /usr/bin/time -p qemu-system-riscv64 -L /opt/riscv-cross/riscv64-unknown-elf -nographic -machine spike -bios "$1"; } 2>&1 )
+OUTPUT=$( { /usr/bin/time -p qemu-system-riscv64 -L /opt/riscv/riscv64-unknown-elf -nographic -machine spike -bios "$1"; } 2>&1 )
 RET=$?
 echo $OUTPUT | sed -nE 's/real[[:space:]]*([0-9]+)\.([0-9]+).*/TIME=\1.\2/p'
 
--- vadl/test/resources/images/lcb_execution_test_aarch64/base_image/lcb_wrapper.sh
+++ vadl/test/resources/images/lcb_execution_test_aarch64/base_image/lcb_wrapper.sh
@@ -4,7 +4,7 @@ set -x
 
 /src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -march=${TARGET} -fPIC -c -O0 /tmp/main.s -o /tmp/main.o
 /src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -march=${TARGET} -c -O0 /helper/init.S -o /helper/init.o
-/opt/riscv-cross/bin/riscv64-unknown-elf-gcc -march=${TARGET} -mabi=${ABI} -static -nostartfiles -T/helper/link.ld /tmp/main.o /helper/init.o -o /tmp/main
+/opt/riscv/bin/riscv64-unknown-elf-gcc -march=${TARGET} -mabi=${ABI} -static -nostartfiles -T/helper/link.ld /tmp/main.o /helper/init.o -o /tmp/main
 
 echo "Running spike..."
-qemu-system-${UPSTREAM_CLANG_TARGET} -L /opt/riscv-cross/riscv64-unknown-elf -nographic -machine spike -bios /tmp/main
+qemu-system-${UPSTREAM_CLANG_TARGET} -L /opt/riscv/riscv64-unknown-elf -nographic -machine spike -bios /tmp/main
