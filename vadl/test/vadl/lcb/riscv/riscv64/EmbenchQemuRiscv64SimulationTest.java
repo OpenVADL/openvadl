d27e95ca49386ad409a8fef2aec7f373d155d734
Kevin Per <kevin.per@protonmail.com>
2025-05-28 05:58:08 +0000

    lcb: Updated instruction for riscv32

--- vadl/test/vadl/lcb/riscv/riscv64/EmbenchQemuRiscv64SimulationTest.java
+++ vadl/test/vadl/lcb/riscv/riscv64/EmbenchQemuRiscv64SimulationTest.java
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package vadl.lcb.riscv;
+package vadl.lcb.riscv.riscv64;
 
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
