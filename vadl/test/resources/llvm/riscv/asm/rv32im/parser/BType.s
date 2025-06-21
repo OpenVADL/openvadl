6b091b3b80e8c39af3917312eb674e6966b5e7db
Kevin Per <kevin.per@protonmail.com>
2025-06-21 07:49:45 +0000

    lcb: Updated tests

--- vadl/test/resources/llvm/riscv/asm/rv32im/parser/BType.s
+++ vadl/test/resources/llvm/riscv/asm/rv32im/parser/BType.s
@@ -1,33 +1,15 @@
 # RUN: /src/llvm-final/build/bin/llvm-mc -arch=rv32im -show-inst < $INPUT | /src/llvm-final/build/bin/FileCheck $INPUT
 
-BEQ x1, x2, 1
-# CHECK: <MCInst #{{[0-9]+}} BEQ
-# CHECK-NEXT: <MCOperand Reg:3>
-# CHECK-NEXT: <MCOperand Reg:4>
-# CHECK-NEXT: <MCOperand Imm:0>>
-
 BNE x3, x4, 2
 # CHECK: <MCInst #{{[0-9]+}} BNE
 # CHECK-NEXT: <MCOperand Reg:5>
 # CHECK-NEXT: <MCOperand Reg:6>
 # CHECK-NEXT: <MCOperand Imm:2>>
 
-BGE x5, x6, 3
-# CHECK: <MCInst #{{[0-9]+}} BGE
-# CHECK-NEXT: <MCOperand Reg:7>
-# CHECK-NEXT: <MCOperand Reg:8>
-# CHECK-NEXT: <MCOperand Imm:2>>
-
 BGEU x7, x8, 4
 # CHECK: <MCInst #{{[0-9]+}} BGEU
 # CHECK-NEXT: <MCOperand Reg:9>
 # CHECK-NEXT: <MCOperand Reg:10>
-# CHECK-NEXT: <MCOperand Imm:4>>
-
-BLT x9, x10, 5
-# CHECK: <MCInst #{{[0-9]+}} BLT
-# CHECK-NEXT: <MCOperand Reg:11>
-# CHECK-NEXT: <MCOperand Reg:12>
 # CHECK-NEXT: <MCOperand Imm:4>>
 
 BLTU x11, x12, 6
