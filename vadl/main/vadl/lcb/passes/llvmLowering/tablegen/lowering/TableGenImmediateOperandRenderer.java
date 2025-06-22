e30ab3c1ddf39dccd386cfdbb26d921378ab0581
Kevin Per <kevin.per@protonmail.com>
2025-06-22 05:59:34 +0000

    lcb: Removed case distinction for predicates when rendering

--- vadl/main/vadl/lcb/passes/llvmLowering/tablegen/lowering/TableGenImmediateOperandRenderer.java
+++ vadl/main/vadl/lcb/passes/llvmLowering/tablegen/lowering/TableGenImmediateOperandRenderer.java
@@ -19,7 +19,6 @@ package vadl.lcb.passes.llvmLowering.tablegen.lowering;
 
 import java.util.Map;
 import org.apache.commons.text.StringSubstitutor;
-import vadl.gcb.passes.GenerateValueRangeImmediatePass;
 import vadl.lcb.passes.llvmLowering.tablegen.model.TableGenImmediateRecord;
 
 /**
@@ -31,48 +30,6 @@ public final class TableGenImmediateOperandRenderer {
    * Transforms the given {@code operand} into a string which can be used by LLVM's TableGen.
    */
   public static String lower(TableGenImmediateRecord operand) {
-    if (operand.fieldAccessRef().fieldRefs().size() == 1) {
-      return handleCaseOneField(operand);
-    } else {
-      return handleCaseMultipleFields(operand);
-    }
-  }
-
-  private static String handleCaseOneField(TableGenImmediateRecord operand) {
-    var field = operand.fieldAccessRef().fieldRefs().getFirst();
-    var ty = field.type().toBitsType();
-    var isSigned = operand.fieldAccessRef().type().asDataType().isSigned();
-    var highestPossibleValue =
-        GenerateValueRangeImmediatePass.highestPossibleValue(ty, isSigned);
-    var lowestPossibleValue =
-        GenerateValueRangeImmediatePass.lowestPossibleValue(ty, isSigned);
-
-    return StringSubstitutor.replace("""
-            class ${rawName}<ValueType ty> : Operand<ty>
-            {
-              let EncoderMethod = "${encoderMethod}";
-              let DecoderMethod = "${decoderMethod}";
-            }
-            
-            def ${fullName}
-                : ${rawName}<${type}>
-                , ImmLeaf<${type}, [{ return Imm >= ${lowestPossibleValue}
-                  && Imm <= ${highestPossibleValue}
-                  && ${predicateMethod}(Imm); }]>;
-            
-            def ${rawName}AsLabel : ${rawName}<OtherVT>;
-            """,
-        Map.of("rawName", operand.rawName(),
-            "fullName", operand.fullname(),
-            "encoderMethod", operand.encoderMethod().lower(),
-            "decoderMethod", operand.decoderMethod().lower(),
-            "type", operand.llvmType().getLlvmType(),
-            "lowestPossibleValue", lowestPossibleValue,
-            "highestPossibleValue", highestPossibleValue,
-            "predicateMethod", operand.predicateMethod().lower()));
-  }
-
-  private static String handleCaseMultipleFields(TableGenImmediateRecord operand) {
     return StringSubstitutor.replace("""
             class ${rawName}<ValueType ty> : Operand<ty>
             {
