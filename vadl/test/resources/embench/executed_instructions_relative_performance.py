940cc62c84d89f52f54a96008c22d74d51344e1d
Kevin Per <kevin.per@protonmail.com>
2025-08-07 16:42:44 +0000

    lcb: Fixed bug in python script

--- vadl/test/resources/embench/executed_instructions_relative_performance.py
+++ vadl/test/resources/embench/executed_instructions_relative_performance.py
@@ -13,4 +13,4 @@ p["rel"] = p['upstream'] / p['lcb']
 p.to_csv('../result/executed_instructions_relative.csv', header=None, index=False)
 mean = stats.gmean(p["rel"])
 print("Upstream / LCB")
-print("Lcb emits more instructions than upstream: " + str(mean) + "(" + str(1 - mean) + ")"
+print("Lcb emits more instructions than upstream: " + str(mean) + "(" + str(1 - mean) + ")")
