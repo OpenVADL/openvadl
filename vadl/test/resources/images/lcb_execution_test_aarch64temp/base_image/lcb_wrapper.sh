a542b218c01dd688fa9a370c52c9cfe583a5cb36
Kevin Per <kevin.per@protonmail.com>
2025-08-18 09:20:29 +0000

    lcb: Setup UME for AArch64

--- vadl/test/resources/images/lcb_execution_test_aarch64temp/base_image/lcb_wrapper.sh
+++ vadl/test/resources/images/lcb_execution_test_aarch64temp/base_image/lcb_wrapper.sh
@@ -3,8 +3,8 @@
 set -x
 
 /src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -fPIC -c -O0 /tmp/main.s -o /tmp/main.o
-/src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -c -O0 /helper/init.S -o /helper/init.o
-/opt/aarch64/bin/aarch64-none-linux-gnu-gcc -mabi=${ABI} -static -nostartfiles -T/helper/link.ld /tmp/main.o /helper/init.o -o /tmp/main
+#/src/llvm-final/build/bin/clang --target=${UPSTREAM_CLANG_TARGET} -c -O0 /helper/init.S -o /helper/init.o
+/opt/aarch64/bin/aarch64-none-linux-gnu-gcc -mabi=${ABI} -static /tmp/main.o -o /tmp/main
 
 echo "Running spike..."
-qemu-system-${UPSTREAM_CLANG_TARGET} -L /opt/aarch64/aarch64-none-linux-gnu/include -nographic -machine virt -cpu cortex-a57 -m 1024 -bios /tmp/main
+qemu-${UPSTREAM_CLANG_TARGET} -L /opt/aarch64/aarch64-none-linux-gnu/include /tmp/main
