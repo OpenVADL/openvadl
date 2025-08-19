c01e34b6179af758370c7720cfcf2c044b7cfce7
Kevin Per <kevin.per@protonmail.com>
2025-08-19 10:17:42 +0000

    lcb: Fixed build

--- vadl/main/vadl/lcb/passes/llvmLowering/strategies/instruction/LlvmInstructionLoweringUnconditionalJumpWithoutLinkRegistersStrategyImpl.java
+++ vadl/main/vadl/lcb/passes/llvmLowering/strategies/instruction/LlvmInstructionLoweringUnconditionalJumpWithoutLinkRegistersStrategyImpl.java
@@ -96,6 +96,7 @@ public class LlvmInstructionLoweringUnconditionalJumpWithoutLinkRegistersStrateg
         instruction,
         info.withFlags(flags),
         patterns,
+        Collections.emptyList(),
         Collections.emptyList());
   }
 
