ccae4c808fb2780d3e95a19fe5581e917f3e7580
Linus Halder <linus.halder@student.tuwien.ac.at>
2025-09-25 23:47:27 +0200

    rtl: Emit scalafmt config for scala formatting

--- vadl/main/vadl/rtl/passes/EmitRtlMakefilePass.java
+++ vadl/main/vadl/rtl/passes/EmitRtlMakefilePass.java
@@ -16,6 +16,8 @@
 
 package vadl.rtl.passes;
 
+import java.nio.file.Path;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,10 +25,14 @@ import vadl.configuration.RtlConfiguration;
 import vadl.pass.PassName;
 import vadl.pass.PassResults;
 import vadl.rtl.template.RtlTemplateRenderingPass;
+import vadl.template.AbstractMultiTemplateRenderingPass;
 import vadl.viam.InstructionSetArchitecture;
 import vadl.viam.Specification;
 import vadl.viam.graph.dependency.SideEffectNode;
 
+/**
+ * Emit RTL Makefile.
+ */
 public class EmitRtlMakefilePass extends RtlTemplateRenderingPass {
 
   public EmitRtlMakefilePass(RtlConfiguration configuration) {
@@ -44,16 +50,29 @@ public class EmitRtlMakefilePass extends RtlTemplateRenderingPass {
   }
 
   @Override
-  protected boolean enableCopyright() {
-    return false;
+  protected String lineComment() {
+    return "#";
   }
 
   @Override
   protected List<RenderInput> createRenderInputs(PassResults passResults,
                                                  Specification specification,
                                                  Map<String, Object> baseVariables) {
+    // get list of emitted files
+    var renderResults = passResults.allResultsOf(RtlTemplateRenderingPass.class,
+        AbstractMultiTemplateRenderingPass.Result.class);
+
+    var scalaFiles = renderResults
+        .map(AbstractMultiTemplateRenderingPass.Result::emittedFiles)
+        .flatMap(Collection::stream)
+        .filter(file -> file.toString().endsWith(".scala") || file.toString().endsWith(".sbt"))
+        .map(this::relativePathToOutputPath)
+        .map(this::escapePath)
+        .toList();
+
     var vars = new HashMap<String, Object>();
     vars.put("hasEcall", specification.isa().map(this::hasEcall).orElse(false));
+    vars.put("scalaFiles", scalaFiles);
     return List.of(
         new RenderInput("Makefile", mergeVariables(baseVariables, vars))
     );
@@ -64,5 +83,15 @@ public class EmitRtlMakefilePass extends RtlTemplateRenderingPass {
       instr.simpleName().equals("ECALL")
           && instr.behavior().getNodes().anyMatch(SideEffectNode.class::isInstance)
     );
+  }
+
+  private String relativePathToOutputPath(Path path) {
+    var outputPath = Path.of(configuration().outputPath().toString(), subDir);
+    var relPath = outputPath.relativize(path);
+    return relPath.toString();
+  }
+
+  private String escapePath(String filename) {
+    return filename.replace("\\", "/");
   }
 }
