a0ff1a017e37d2964832b82bc43a2c8e0c9b08f7
Jakob Rathbauer <jakob@jaraonthe.net>
2025-10-01 15:35:27 +0200

    lsp: Fix gradle configuration

--- vadl-lsp/build.gradle.kts
+++ vadl-lsp/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 group = "vadl"
 version = "unspecified"
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation(project(":vadl"))
     implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.24.0")
