efe38124b8f1f2247ceba1f7210b6d8b306cd33a
Kevin Per <kevin.per@protonmail.com>
2026-02-09 09:35:03 +0000

    cpp: Removed z3 symbolic tests

--- vadl/test/resources/images/python_z3/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for FILE in /inputs/*; do
-    python3 "$FILE"
-    STATUS_CODE=$?
-
-    # Write the file name and status code to the CSV file
-    echo "$(basename "$FILE"),$STATUS_CODE" >> /work/output.csv
-done
