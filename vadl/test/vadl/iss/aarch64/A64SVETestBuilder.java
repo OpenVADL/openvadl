db86ec56c785801c12db5857c8449d8a5af3d3d5
Johannes Zottele <official@johannes-zottele.at>
2026-02-14 19:50:00 +0100

    iss: Add SVE instruction test scaffolding and basic vector operations

---
+++ vadl/test/vadl/iss/aarch64/A64SVETestBuilder.java
@@ -0,0 +1,66 @@
+// SPDX-FileCopyrightText : © 2026 TU Wien <vadl@tuwien.ac.at>
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package vadl.iss.aarch64;
+
+public class A64SVETestBuilder extends A64TestBuilder {
+
+  public A64SVETestBuilder(String testId) {
+    super(testId);
+  }
+
+  /**
+   * Placeholder hook for SVE setup.
+   * Only emits operations modeled by the current AArch64 spec.
+   */
+  public void configureCpuForSveOps(String tmpReg, int vlBits) {
+    if (vlBits <= 0) {
+      throw new IllegalArgumentException("SVE VL must be > 0, got " + vlBits);
+    }
+    add("# configure CPACR to enable SIMD/SVE access in reference QEMU");
+    add("mov %s, #0x330000", tmpReg);
+    add("msr cpacr_el1, %s", tmpReg);
+  }
+
+  public void fillMemory64(long addr, int words, String addrReg, String dataReg) {
+    add("ldr %s, =0x%x", addrReg, addr);
+    for (int i = 0; i < words; i++) {
+      fillRegUnsigned(dataReg, 64);
+      add("str %s, [%s, #%d]", dataReg, addrReg, i * 8);
+    }
+  }
+
+  public void loadZFromMemory(String zReg, long addr, String addrReg) {
+    add("ldr %s, =0x%x", addrReg, addr);
+    add("ldr %s, [%s]", zReg, addrReg);
+  }
+
+  public void storeZToMemory(String zReg, long addr, String addrReg) {
+    add("ldr %s, =0x%x", addrReg, addr);
+    add("str %s, [%s]", zReg, addrReg);
+  }
+
+  public void loadMemory64ToRegs(long addr, int words, int firstReg, String addrReg) {
+    add("ldr %s, =0x%x", addrReg, addr);
+    for (int i = 0; i < words; i++) {
+      add("ldr x%d, [%s, #%d]", firstReg + i, addrReg, i * 8);
+    }
+  }
+
+  public void setPredicateAllTrue(String pReg, String elemSuffix) {
+    add("ptrue %s.%s", pReg, elemSuffix);
+  }
+}
