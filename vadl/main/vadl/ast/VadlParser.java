6a4cb4284fca6158ad1762eec649c41d8869e86d
Florian Freitag <flohacksfriday@gmail.com>
2026-04-12 23:40:32 +0200

    frontend: Add internal broken parser api (#867)
    This API is mostly for the LSP, @jaraonthe-dot-net can you verify that
    this would be helpful for you?
    
    There is also a commit in here that cleans up the parser API and removes
    a couple of barely used functions.
    
    This PR is the successor to #838

--- vadl/main/vadl/ast/VadlParser.java
+++ vadl/main/vadl/ast/VadlParser.java
@@ -37,6 +37,11 @@ public class VadlParser {
 
   /**
    * Parses the VADL source program at the specified path into an AST.
+   * The returned AST is already macro expanded and all symbols are resolved.
+   * This method loads the file always from disk.
+   *
+   * @param path to load the file from.
+   * @throws DiagnosticList if the file cannot be loaded or not correctly parsed.
    */
   public static Ast parse(Path path) {
     return parse(path, new DiskVirtualFileSystem(), Collections.emptyMap());
@@ -44,6 +49,12 @@ public class VadlParser {
 
   /**
    * Parses the VADL source program at the specified path into an AST.
+   * The returned AST is already macro expanded and all symbols are resolved.
+   * The file will be loaded from the specified filesystem.
+   *
+   * @param path to load the file from.
+   * @param fileSystem to load the file from.
+   * @throws DiagnosticList if the file cannot be loaded or not correctly parsed.
    */
   public static Ast parse(Path path, VirtualFileSystem fileSystem) {
     return parse(path, fileSystem, Collections.emptyMap());
@@ -51,8 +62,27 @@ public class VadlParser {
 
   /**
    * Parses the VADL source program at the specified path into an AST.
-   * Works just like {@link VadlParser#parse(String, Map, Path)},
-   * except errors will have the proper file locations set.
+   * The returned AST is already macro expanded and all symbols are resolved.
+   * This method is most useful for testing.
+   *
+   * @param program to parse.
+   * @throws DiagnosticList if the file cannot be loaded or not correctly parsed.
+   */
+  public static Ast parse(String program) {
+    var path = Paths.get("memory");
+    var fileSystem = new SingleFileVirtualFileSystem(program, path);
+    return parse(path, fileSystem);
+  }
+
+  /**
+   * Parses the VADL source program at the specified path into an AST.
+   * The returned AST is already macro expanded and all symbols are resolved.
+   * The file will be loaded from the specified filesystem.
+   *
+   * @param path to load the file from.
+   * @param fileSystem to load the file from.
+   * @param macroOverrides which are either specified from an import or the CLI.
+   * @throws DiagnosticList if the file cannot be loaded or not correctly parsed.
    */
   public static Ast parse(Path path, VirtualFileSystem fileSystem,
                           Map<String, String> macroOverrides) {
@@ -66,36 +96,6 @@ public class VadlParser {
     ast.filePath = fileSystem.toAbsolutePath(path);
     return ast;
   }
-
-  /**
-   * Convenience overload for {@link VadlParser#parse(String, Map, Path)} without any overrides.
-   */
-  public static Ast parse(String program) {
-    return parse(program, Map.of(), Paths.get("memory"));
-  }
-
-  /**
-   * Convenience overload for {@link VadlParser#parse(String, Map, Path)} without any overrides.
-   */
-  public static Ast parse(String program, Path resolutionPath) {
-    return parse(program, Map.of(), resolutionPath);
-  }
-
-  /**
-   * Parses a source program into an AST.
-   *
-   * @param program         a source code file to parse
-   * @param macroOverrides  The overrides to perform in the macro evaluation
-   * @return                The parsed syntax tree.
-   * @throws DiagnosticList if there are any parsing errors.
-   */
-  public static Ast parse(String program, Map<String, String> macroOverrides,
-                          @Nullable Path resolutionPath) {
-    var path = Paths.get("virtualFile.vadl");
-    var fileSystem = new SingleFileVirtualFileSystem(program, path);
-    return parse(path, fileSystem, macroOverrides);
-  }
-
 
   private static Ast parse(Parser parser) {
     List<Diagnostic> errors = new ArrayList<>();
@@ -130,4 +130,79 @@ public class VadlParser {
     return ast;
   }
 
+  /**
+   * A structure to hold an ast and the errors that might have been produced during its creation.
+   *
+   * @param ast that was parsed.
+   * @param diagnostics null if everything went ok, otherwise a non-empty list.
+   */
+  record PotentiallyBrokenAst(
+      Ast ast,
+      @Nullable DiagnosticList diagnostics) {
+    public boolean isBroken() {
+      return diagnostics != null;
+    }
+  }
+
+  /**
+   * Parses the VADL source program at the specified path into an AST that might not have succeeded
+   * parsing. Even if parsing fails all symbols tried to be resolved. The errors returned are from
+   * the first stage that fails to avoid follow-up errors.
+   *
+   * <p>USE WITH EXTREME CAUTION! There are no guarantees about the state in which the AST might
+   * be in. This includes, but is not limited to, dummy statements that don't appear in the input
+   * but are inserted as placeholders and still contained as macro expansion has gone wrong, only
+   * halfway parsed inputs as faulty macro invocation prevents the parser from continuing.
+   *
+   * @param path           to load the file from.
+   * @param fileSystem     to load the file from.
+   * @param macroOverrides which are either specified from an import or the CLI.
+   */
+  public static PotentiallyBrokenAst unsafeParseToPotentiallyBrokenAst(
+      // FIXME: This is really similar to the normal parse method and so in the future if this
+      // stays that similar we can reabse them on a common core.
+      // https://github.com/OpenVADL/openvadl/pull/867#discussion_r3037098145
+      Path path,
+      VirtualFileSystem fileSystem, Map<String, String> macroOverrides) {
+
+    var scanner = new Scanner(fileSystem.getInputStream(path));
+    var parser = new Parser(scanner);
+    parser.sourceFile = fileSystem.toAbsolutePath(path);
+    parser.fileSystem = fileSystem;
+    macroOverrides.forEach((key, value) -> parser.macroOverrides.put(key,
+        new Identifier(value, SourceLocation.INVALID_SOURCE_LOCATION)));
+    var potentialBrokenAst =
+        parser.ast.withPassTiming("Parsing", () -> unsafeParseToPotentiallyBrokenAst(parser));
+    potentialBrokenAst.ast.filePath = fileSystem.toAbsolutePath(path);
+    return potentialBrokenAst;
+  }
+
+  private static PotentiallyBrokenAst unsafeParseToPotentiallyBrokenAst(Parser parser) {
+    List<Diagnostic> errors = new ArrayList<>();
+
+    try {
+      parser.Parse();
+    } catch (Diagnostic e) {
+      errors.add(e);
+    } catch (DiagnosticList e) {
+      errors.addAll(e.items);
+    }
+
+    errors.addAll(parser.diagnostics);
+
+
+    // during parsing there might be errors in the macro table (e.g. conflicting macro definitions)
+    if (errors.isEmpty()) {
+      errors = parser.macroTable.errors.stream().distinct().toList();
+    }
+
+    var ast = parser.ast;
+
+    var symbolErrors = SymbolTable.collectAndResolveSymbols(ast);
+    if (errors.isEmpty()) {
+      errors = symbolErrors;
+    }
+
+    return new PotentiallyBrokenAst(ast, !errors.isEmpty() ? new DiagnosticList(errors) : null);
+  }
 }
