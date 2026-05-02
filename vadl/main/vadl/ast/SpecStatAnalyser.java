f5b73a4874ea0dc1e838001e68ca11e4ec241b3a
Florian Freitag <flohacksfriday@gmail.com>
2026-05-02 13:36:29 +0200

    frontend: Refactor spec stats analysis (#922)

--- vadl/main/vadl/ast/SpecStatAnalyser.java
+++ vadl/main/vadl/ast/SpecStatAnalyser.java
@@ -17,63 +17,172 @@
 package vadl.ast;
 
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import vadl.utils.SourceLocation;
 import vadl.utils.VirtualFileSystem;
+import vadl.utils.WithLocation;
 
 /**
  * An AST analyzer that collects statistics about the content of the specification.
+ * This can be useful for publications to let the reader estimate the size of the specification.
  */
 public class SpecStatAnalyser extends RecursiveAstVisitor {
 
-  private static final SpecStats stats = new SpecStats();
   private final VirtualFileSystem fileSystem;
+  private final Set<SourceLocation> seenUnexpandedLocations = new HashSet<>();
+  private int files = 0;
+  private int linesOfCode = 0;
+  private int functionDefinitions = 0;
+  private int formatDefinitions = 0;
+  private int instructionDefinition = 0;
+  private int totalDefinitions = 0;
+  private int totalStatements = 0;
+  private int totalExpressions = 0;
+  private int modelDefinitions = 0;
+  private int recordDefinitions = 0;
+  private int modelTypeDefinitions = 0;
+  private int functionDefinitionsUnexpanded = 0;
+  private int formatDefinitionsUnexpanded = 0;
+  private int instructionDefinitionUnexpanded = 0;
+  private int totalDefinitionsUnexpanded = 0;
+  private int totalStatementsUnexpanded = 0;
+  private int totalExpressionsUnexpanded = 0;
 
   private SpecStatAnalyser(VirtualFileSystem fileSystem) {
     this.fileSystem = fileSystem;
   }
 
   /**
-   * Runs the analyser on the given AST and returns the collected statistics.
+   * Runs the analyzer on the given AST and returns the collected statistics.
    *
-   * @param ast to analyze.
-   * @param fileSystem from which the files can be read again to calcuulate the lines of code.
+   * @param ast        to analyze (should already have all models removed).
+   * @param fileSystem from which the files can be read again to calculate the lines of code.
    * @return the collected statistics.
    */
-  public static SpecStats run(Ast ast, VirtualFileSystem fileSystem) {
+  public static List<SpecStat> run(Ast ast, VirtualFileSystem fileSystem) {
     var analyser = new SpecStatAnalyser(fileSystem);
     ast.definitions.forEach(definition -> definition.accept(analyser));
+    analyser.addModelNodes(ast.rootSymbolTable());
 
     ast.allReadFiles().forEach(analyser::handlePath);
 
-    return analyser.stats;
+    return List.of(
+        new SpecStat("Files", analyser.files),
+        new SpecStat("Lines of code", analyser.linesOfCode),
+        new SpecStat("Model Definitions", analyser.modelDefinitions),
+        new SpecStat("Record Definitions", analyser.recordDefinitions),
+        new SpecStat("Model-Type Definitions", analyser.modelTypeDefinitions),
+        new SpecStat("Function Definitions Unexpanded", analyser.functionDefinitionsUnexpanded),
+        new SpecStat("Format Definitions Unexpanded", analyser.formatDefinitionsUnexpanded),
+        new SpecStat("Instruction Definitions Unexpanded",
+            analyser.instructionDefinitionUnexpanded),
+        new SpecStat("Total Definitions Unexpanded", analyser.totalDefinitionsUnexpanded),
+        new SpecStat("Total Statements Unexpanded", analyser.totalStatementsUnexpanded),
+        new SpecStat("Total Expressions Unexpanded", analyser.totalExpressionsUnexpanded),
+        new SpecStat("Function Definitions", analyser.functionDefinitions),
+        new SpecStat("Format Definitions", analyser.formatDefinitions),
+        new SpecStat("Instruction Definitions", analyser.instructionDefinition),
+        new SpecStat("Total Definitions", analyser.totalDefinitions),
+        new SpecStat("Total Statements", analyser.totalStatements),
+        new SpecStat("Total Expressions", analyser.totalExpressions)
+
+    );
   }
 
   private void handlePath(Path path) {
-    stats.files++;
-    fileSystem.readLines(path).forEach(line -> stats.linesOfCode++);
+    files++;
+    fileSystem.readLines(path).forEach(unused -> linesOfCode++);
+  }
+
+  /**
+   * Execute a runnable if the node is contained in the unexpanded spec.
+   * So obviously this is a bit tricky because the parser already expands all macros so and there
+   * is no parser that can tell us if which node were in the original spec.
+   * However, expanded nodes always have the same location with only differing in the
+   * expanded from field. So if we ignore that field, we can calculate the
+   * unexpanded node count by only taking each location once. So we track them in
+   * {@link #seenUnexpandedLocations} and if we find a new one that isn't in there already, it must
+   * have also been in the unexpanded form once. And if we have already seen it, it was from a
+   * macro, and we already have added it once so we can ignore it.
+   *
+   * @param loctable on which it will be decided if we have seen this node before.
+   * @param action   that will be executed if the node hasn't been seen before.
+   * @return true if the node was new and action was executed, false otherwise.
+   */
+  private boolean ifNewUnexpanded(WithLocation loctable, Runnable action) {
+    var directLocation = new SourceLocation(loctable.location().path(), loctable.location().begin(),
+        loctable.location().end());
+    var isNew = !seenUnexpandedLocations.contains(directLocation);
+    if (isNew) {
+      seenUnexpandedLocations.add(directLocation);
+      action.run();
+    }
+    return isNew;
+  }
+
+  /**
+   * Counts the number of models and model-related types.
+   * This again seems hard because {@link ModelRemover} already has removed all the models from the
+   * AST. However, since models are scoped, they get entered into the symbolTables and never removed
+   * from them. So we can traverse all symbol tables of all definitions and count the models.
+   *
+   * @param table of a definition to import all the macros from.
+   */
+  private void addModelNodes(SymbolTable table) {
+    table.macroSymbols.values().forEach(symbol -> {
+      var node = symbol.origin();
+      ifNewUnexpanded(node, () -> {
+        if (node instanceof ModelDefinition) {
+          modelDefinitions++;
+        } else if (node instanceof RecordTypeDefinition) {
+          recordDefinitions++;
+        } else if (node instanceof ModelTypeDefinition) {
+          modelTypeDefinitions++;
+        }
+      });
+    });
   }
 
   @Override
   public void beforeTravel(Definition definition) {
-    stats.totalDefinitions++;
+    totalDefinitions++;
+    var isNew = ifNewUnexpanded(definition, () -> totalDefinitionsUnexpanded++);
     if (definition instanceof FunctionDefinition) {
-      stats.functionDefinitions++;
+      functionDefinitions++;
+      if (isNew) {
+        functionDefinitionsUnexpanded++;
+      }
     }
     if (definition instanceof FormatDefinition) {
-      stats.formatDefinitions++;
+      formatDefinitions++;
+      if (isNew) {
+        formatDefinitionsUnexpanded++;
+      }
     }
     if (definition instanceof InstructionDefinition) {
-      stats.instructionDefinition++;
+      instructionDefinition++;
+      if (isNew) {
+        instructionDefinitionUnexpanded++;
+      }
+    }
+
+    if (definition.symbolTable != null) {
+      addModelNodes(definition.symbolTable());
     }
   }
 
   @Override
   public void beforeTravel(Statement statement) {
-    stats.totalStatements++;
+    totalStatements++;
+    ifNewUnexpanded(statement, () -> totalStatementsUnexpanded++);
   }
 
   @Override
   public void beforeTravel(Expr expr) {
-    stats.totalExpressions++;
+    totalExpressions++;
+    ifNewUnexpanded(expr, () -> totalExpressionsUnexpanded++);
   }
 
   @Override
@@ -87,15 +196,7 @@ public class SpecStatAnalyser extends RecursiveAstVisitor {
   /**
    * Statistics container to hold the collected data.
    */
-  public static class SpecStats {
-    public int files = 0;
-    public int linesOfCode = 0;
-    public int functionDefinitions = 0;
-    public int formatDefinitions = 0;
-    public int instructionDefinition = 0;
-    public int totalDefinitions = 0;
-    public int totalStatements = 0;
-    public int totalExpressions = 0;
+  public record SpecStat(String name, int count) {
   }
 
 }
