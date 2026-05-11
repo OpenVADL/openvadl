ddccd92e44043e3f5c9674f73cbc3c49dd92c3e3
flofriday <flohacksfriday@gmail.com>
2026-05-11 16:35:56 +0200

    vadl: Refactor expanded location comparison

--- vadl/main/vadl/utils/SourceLocation.java
+++ vadl/main/vadl/utils/SourceLocation.java
@@ -152,9 +152,13 @@ public sealed interface SourceLocation extends WithLocation, Comparable<SourceLo
     }
 
     // Shortcut if both have the same expansion stack
-    if (this.expandedFromStack().equals(other.expandedFromStack())) {
-      return new ExpandedLocation(new DirectLocation(this.path(), this.begin(), other.end()),
-          RopeList.of(this.expandedFromStack()));
+    if (this instanceof ExpandedLocation thisExpandedLoc
+        && other instanceof ExpandedLocation otherExpandedLoc
+        && thisExpandedLoc.expandedFrom.equals(otherExpandedLoc.expandedFrom)) {
+      var begin = this.begin().compareTo(other.begin()) < 0 ? this.begin() : other.begin();
+      var end = this.end().compareTo(other.end()) > 0 ? this.end() : other.end();
+      return new ExpandedLocation(new DirectLocation(this.path(), begin, end),
+          thisExpandedLoc.expandedFrom);
     }
 
     // The expansion stacks differ, let's find a shared substack or use the innermost invocation.
