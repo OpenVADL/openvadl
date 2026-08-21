4addbb4cd33978b78c02a25f50712630778259b1
arcane-quill <meow.meow.meow647737@gmail.com>
2026-08-21 12:11:09 +0200

    ume: Add anon syscall exception #757

--- vadl/main/vadl/iss/passes/tcg/IssHardcodedTcgAddOnPass.java
+++ vadl/main/vadl/iss/passes/tcg/IssHardcodedTcgAddOnPass.java
@@ -22,10 +22,21 @@ import java.util.function.Consumer;
 import javax.annotation.Nullable;
 import vadl.configuration.IssConfiguration;
 import vadl.iss.passes.AbstractIssPass;
+import vadl.iss.passes.extensions.ExceptionInfo;
+import vadl.iss.passes.tcg.lowering.nodes.TcgGenException;
 import vadl.pass.PassName;
 import vadl.pass.PassResults;
+import vadl.utils.SourceLocation;
+import vadl.viam.ExceptionDef;
+import vadl.viam.Identifier;
 import vadl.viam.Instruction;
+import vadl.viam.Parameter;
 import vadl.viam.Specification;
+import vadl.viam.graph.Graph;
+import vadl.viam.graph.NodeList;
+import vadl.viam.graph.control.InstrEndNode;
+import vadl.viam.graph.control.ProcEndNode;
+import vadl.viam.graph.control.StartNode;
 
 /**
  * This pass manipulates the VIAM with hardcoded elements.
@@ -33,6 +44,27 @@ import vadl.viam.Specification;
  * this is not yet supported in the VADL specification.
  */
 public class IssHardcodedTcgAddOnPass extends AbstractIssPass {
+
+  private static Graph createDummySyscallGraph() {
+    var g = new Graph("synthetic-syscall");
+    g.setSourceLocation(SourceLocation.INVALID_SOURCE_LOCATION);
+
+    var end = g.addWithInputs(new ProcEndNode(new NodeList<>()));
+    end.setSourceLocation(SourceLocation.INVALID_SOURCE_LOCATION);
+
+    var start = new StartNode(end);
+    start.setSourceLocation(SourceLocation.INVALID_SOURCE_LOCATION);
+    g.addWithInputs(start);
+
+    return g;
+  }
+
+  private static final ExceptionDef SYSCALL_EXC = new ExceptionDef(
+      new Identifier(List.of("Syscall"), SourceLocation.INVALID_SOURCE_LOCATION),
+      new Parameter[]{},
+      createDummySyscallGraph(),
+      ExceptionDef.Kind.ANONYMOUS
+  );
 
   public IssHardcodedTcgAddOnPass(IssConfiguration configuration) {
     super(configuration);
@@ -43,17 +75,32 @@ public class IssHardcodedTcgAddOnPass extends AbstractIssPass {
     return PassName.of("ISS Hardcoded TCG Add-Ons");
   }
 
-  List<Consumer<Instruction>> instrAddOns = List.of(
+  Consumer<Instruction> injectSyscallRaise = instr -> {
 
+    if (!"ECALL".equalsIgnoreCase(instr.simpleName())) {
+      return;
+    }
+
+    var graph = instr.behavior();
+
+    var instrEnd = graph.getNodes(InstrEndNode.class).findFirst().orElseThrow();
+
+    instrEnd.addBefore(new TcgGenException(SYSCALL_EXC, new NodeList<>()));
+  };
+
+  List<Consumer<Instruction>> instrAddOns = List.of(
+      injectSyscallRaise
   );
 
   @Override
   public @Nullable Object execute(PassResults passResults, Specification viam)
       throws IOException {
 
-    normalTcgInstrs(viam)
-        .forEach(i ->
-            instrAddOns.forEach(f -> f.accept(i)));
+    var isa = viam.isa().orElseThrow();
+    var excInfo = isa.expectExtension(ExceptionInfo.class);
+    excInfo.addException(SYSCALL_EXC);
+
+    normalTcgInstrs(viam).forEach(i -> instrAddOns.forEach(f -> f.accept(i)));
 
     return null;
   }
